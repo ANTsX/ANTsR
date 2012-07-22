a11e5e8c9522196569ed93196dec85047ad11be9
Shrinidhi KL <nidhi84shri@gmail.com>
2012-07-22 18:38:55 -0400

    cbf computation from Chen 2011 paper

---
+++ R/cbf_pasl2.R
@@ -0,0 +1,27 @@
+# chen 2011 paper PASL
+# --------------------------------------------------------------------------------------
+cbf_pasl2 <- function( aslimg_filename , m0img_filename )
+{
+	img <- as.array( antsImageRead( aslimg_filename , "double" , 4 ) )
+	M0 <- as.array( antsImageRead( m0img_filename , "double" , 3 ) )
+
+	numdiffs <- floor( dim(img)[4] / 2 )
+
+	labelimg <- img[ , , , seq( 1 , by = 2 , length.out = numdiffs ) ]
+	controlimg <- img[ , , , seq( 2 , by = 2 , length.out = numdiffs ) ]
+
+	lambda <- 0.9
+	deltaM <- ( controlimg - labelimg )
+	alpha <- 0.95
+
+	M0 <- rep( M0 , numdiffs )
+	dim( M0 ) <- dim( controlimg )
+
+	TI1 <- 700
+	TI2 <- 1700
+	T1b <- 1664
+
+	cbf <- ( lambda * deltaM ) / ( 2 * alpha * M0 * TI1 * exp( -TI2 / T1b ) )
+
+	return( cbf )
+}
