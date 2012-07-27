c5bd5f1bb3ab8a47e62dcd375e39bace2480738f
Shrinidhi KL <nidhi84shri@gmail.com>
2012-07-27 14:27:36 -0400

    vectorized the computation and renamed the R function

--- R/cbf_casl4.R
+++ R/cbf_casl4.R
@@ -1,32 +1,28 @@
 # Wang 2012 paper CASL ; paper does not mention the parameters so using them form the reference [25] mentioned in the paper
 # --------------------------------------------------------------------------------------
-cbf_casl4 <- function( aslimg_filename , Xvar )
+cbf_casl_Wang2012 <- function( aslimg_filename , Xvar )
 {
-	img <- as.array( antsImageRead(  aslimg_filename , "double" , 4 ) )
+	Y <- as.array( antsImageRead( aslimg_filename , "double" , 4 ) )
+	dimY <- dim( Y )
+	dim( Y ) <- c( dimY[1]*dimY[2]*dimY[3] , dimY[4] )
+	Y <- t( Y )
 
-	numdiffs <- floor( dim(img)[4] / 2 )
 	Xideal <- c( -0.5 , 0.5 )
-	Xideal <- rep( Xideal , length.out = dim(img)[4] )
-	cbfform <- formula( Y ~ Xideal + Xvar )
+	Xideal <- rep( Xideal , length.out = dimY[4] )
+	cbfmodel <- lm( Y ~ Xideal + Xvar )
+	Bideal <- (cbfmodel$coefficients)[ 2 , ]
+	dim( Bideal ) <- dimY[1:3]
 
-	cbf <- array( 0 , dim(img)[1:3] )
 	lambda <- 0.9     # stolen
 	T1a <- 1.6        # stolen
 	R1a <- 1 / T1a    # stolen
 	w <- 1
 	alpha <- 0.95     # stolen
 	tau <- 2
+	c <- colMeans( Y )
+	dim( c ) <- dimY[1:3]
 
-	for( i in 1:(dim(img)[1]) )
-	     for( j in 1:(dim(img)[2]) )
-	     	  for( k in 1:(dim(img)[3]) )
-		  {
-			Y <- img[ i , j , k , ]
-			cbfmodel <- lm( cbfform )
-			c <- mean( Y )
-			bideal <- (cbfmodel$coefficients)[2]
-			cbf[ i , j , k ] <- ( bideal * lambda * R1a * exp( w * R1a ) ) / ( 2 * c * alpha * ( 1 - exp( -tau * R1a ) ) )
-		  }
+	cbf <- ( Bideal * lambda * R1a * exp( w * R1a ) ) / ( 2 * c * alpha * ( 1 - exp( -tau * R1a ) ) )
 
 	return( cbf )
 }
