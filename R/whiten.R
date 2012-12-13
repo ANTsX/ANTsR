2190f02419ca447fc588888cdada31576ec0be23
brian avants <stnava@gmail.com>
2012-12-13 10:09:43 -0500

    ENH: add usage to whiten

--- R/whiten.R
+++ R/whiten.R
@@ -1,5 +1,10 @@
 whiten <- function( x )
   {
+  if ( nargs() == 0 )
+    {
+    print("Usage:  x_whitened<-whiten( x ) ")
+    return(1)
+    }
   svdx<-svd( x %*% t(x) )
   dd<-(svdx$d)^(-1/2)
   xw<-( (svdx$u %*% diag(dd) ) %*% t(svdx$v) ) %*% x
