923e32933d9dc3c147bd691998af84468092a86f
brian avants <stnava@gmail.com>
2012-12-14 16:28:48 -0500

    BUG: SmoothImage now replaced with ImageMath version

--- R/SmoothImage.R
+++ R/SmoothImage.R
@@ -1,3 +1,11 @@
-SmoothImage <- function(...){
-  .Call( "SmoothImage", int_antsProcessArguments( c(...) ) ) ;
+SmoothImage <- function( ... ){
+  numargs<-nargs()
+  if ( numargs < 4 )
+    {
+    print("Usage: SmoothImage Dimensionality InImage SmoothingFactor OutImage ")
+    return(0);
+    }
+   args <- list(...)
+   newargs<-list( args[[1]] , args[[4]], "G", args[[2]], args[[3]])
+  .Call( "ImageMath", int_antsProcessArguments( c(newargs) ) ) ;
 }
