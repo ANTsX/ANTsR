257cd4b097f0bef6a400eb0183ed785f486970be
Nick Tustison <ntustison@gmail.com>
2013-01-12 23:27:02 -0500

    BUG:  Didn't add the input path

--- demo/simple_roi_population_study.R
+++ demo/simple_roi_population_study.R
@@ -1,6 +1,6 @@
 library( ANTsR )
 
-roiResults <- simple_roi_analysis( dimensionality = 2, inputPath = "./example_images",
+roiResults <- simple_roi_analysis( dimensionality = 2, inputPath = "./example_images/",
   controlsFileNamePrefix = "phantomtemplate_CONTROL",
   experimentalsFileNamePrefix = "phantomtemplate_EXP",
-  roiLabelsFileName = paste( inputPath, "phantomtemplate_roi_labels.nii.gz", sep = '' ) )
+  roiLabelsFileName = "./example_images/phantomtemplate_roi_labels.nii.gz" )
