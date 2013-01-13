ea07f032ef3b62d8db391fef1b9007fd6168b5b5
brian avants <stnava@gmail.com>
2013-01-12 21:05:20 -0500

    WIP: phantom rework

---
+++ demo/plotANTsImage.R
@@ -0,0 +1,13 @@
+
+
+fn<-"../../demo/example_images/phantomtemplate.jpg"
+img<-antsImageRead(fn,'float',2)
+plotANTsImage(myantsimage=img)
+
+
+
+simg<-antsImageClone(img)
+SmoothImage(2,img,3,simg)
+plotANTsImage(myantsimage=img,functional=simg,threshold="35x150",color="red",axis=1)
+
+
