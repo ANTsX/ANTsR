a19c596a0bd767e017cc3cdf56c8d2e474c35740
brian avants <stnava@gmail.com>
2013-01-22 14:03:08 -0500

    BUG: needed a sep= in antsRegistration

--- R/antsRegistration.R
+++ R/antsRegistration.R
@@ -46,8 +46,8 @@ antsRegistration <- function( fixed = NA, moving = NA, typeofTransform="",outpre
         }
         if ( typeofTransform == "Rigid" | typeofTransform == "Affine" ) {
         args<-list("-d",as.character(fixed@dimension),"-r",paste("[",f,",",m,",1]",sep=''),"-m",paste("mattes[",f,",",m,",1,32,regular,0.2]",sep=''),"-t",paste(typeofTransform,"[0.25]",sep=''),"-c","2100x1200x1200x0","-s","3x2x1x0","-f", "4x3x2x1" ,"-u","1","-z","1","-o",  paste("[",outprefix,",",wmo,",",wfo,"]",sep='') )
-        fwdtransforms<-c( paste(outprefix,"0GenericAffine.mat") )
-        invtransforms<-c( paste(outprefix,"0GenericAffine.mat") )
+        fwdtransforms<-c( paste(outprefix,"0GenericAffine.mat",sep='') )
+        invtransforms<-c( paste(outprefix,"0GenericAffine.mat",sep='') )
         }
         .Call( "antsRegistration", int_antsProcessArguments( c(args) ) ) ;
 #        unlink(ffn) 
