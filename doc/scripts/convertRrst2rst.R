30d02711676c43fd11fd77c343ab6f26069a1332
brian avants <stnava@gmail.com>
2013-01-22 12:24:17 -0500

    WIP: prepping for website update

--- doc/scripts/convertRrst2rst.R
+++ doc/scripts/convertRrst2rst.R
@@ -7,8 +7,8 @@ self<-Args[4]
 self<-substring(self,8,nchar(as.character(self)))
 fn<-(Args[6])
 ofn<-(Args[7])
-# "networkanalysisfMRI.Rrst","restingstatefMRIconnectivity.Rrst",
-myrrst<-c("phantom_population_study.Rrst","plotANTsImage.Rrst","antsSegmentation.Rrst")
+# 
+myrrst<-c("phantom_population_study.Rrst","plotANTsImage.Rrst","antsSegmentation.Rrst","networkanalysisfMRI.Rrst","restingstatefMRIconnectivity.Rrst")
 for ( myfn in myrrst ) {
   rstfn<-sub('.Rrst','.rst',myfn)
   knit(myfn,rstfn)
