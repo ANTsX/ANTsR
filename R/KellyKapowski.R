fbd0a084e815c71eada776e483ac1eb2c0733afb
brian avants <stnava@gmail.com>
2013-01-21 20:23:14 -0500

    ENH: KK implementation & example

--- R/KellyKapowski.R
+++ R/KellyKapowski.R
@@ -1,17 +1,15 @@
-KellyKapowski <- function( kkargs=NA )
+KellyKapowski <- function(  d=NA, s=NA, g=NA, w=NA, c="[45,0.0,10]", r=0.025, m=1.5, outimg=outimg, ... )
 {
-  if ( typeof( kkargs ) != "list" | is.na(kkargs) )
+  if ( missing( d ) | missing( s ) | missing( g ) | missing( w ) | missing( c ) | missing( r ) | missing( m ) | missing( outimg ) )
     {
-    print("Input error - should list params")
+    print("Input error - check params")
     return(NULL)
     }
-  for ( i in c(1:length(kkargs)) )
+  if ( class( s )[1] == "antsImage" )
     {
-      myentry<-kkargs[[i]]
-      if ( names(kkargs)[i] == "s" & class( myentry )[1] == "antsImage" )
-        {
-        kkargs[[i]]<-antsImageClone( kkargs[[i]] , "unsigned int" )
-        }
+    s<-antsImageClone( s , "unsigned int" )
     }
+#               KellyKapowski( d=3, s=simg, g=gimg,w=wimg,c=10,r=0.5,m=1,o=oimg )
+  kkargs<-list( d=d, s=s, g=g, w=w, c=10, r=0.5, m=1, outimg=outimg )
   .Call( "KellyKapowski", int_antsProcessArguments( c(kkargs) ) ) ;
 }
