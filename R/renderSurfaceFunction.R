37e7507a13203114574c3cb1e02dcb2925350986
brian avants <stnava@gmail.com>
2013-01-28 13:48:11 -0500

    ENH: improve getvertices function

--- R/renderSurfaceFunction.R
+++ R/renderSurfaceFunction.R
@@ -13,9 +13,14 @@ renderSurfaceFunction<-function( surfimg, funcimg, surfval=0.5, basefval , offse
   if ( missing( funcimg ) )
     {  cat("just the surface\n")
     surf<-as.array( surfimg )
-    brain <- contour3d(  surf , level = c(surfval), alpha = 1,draw=FALSE,smooth=1,material="metal",depth=0.25,color="white")
+    brain <- contour3d(  surf , level = c(surfval), alpha = 1,draw=FALSE,smooth=1,material="metal",depth=0.6,color="white")
+    # each point has an ID, 3 points make a triangle , the points are laid out as
+    # c( x1 , y1, z1, x2, y2, z2 , ... , xn, yn, zn )
+    # indices are just numbers
+#    vertices<-c(  
+#        brain <-  subdivision3d(brain)
     drawScene.rgl(list(brain))
-    return(NULL)
+    return(list(brain))
     }
   if ( smoothfval > 0 ) {
     SmoothImage(3,funcimg,smoothfval,funcimg)
@@ -40,6 +45,86 @@ renderSurfaceFunction<-function( surfimg, funcimg, surfval=0.5, basefval , offse
     lev<-c(basefval,basefval+offsetfval,basefval+offsetfval*2)
     blob2 <- contour3d( blobi , level = lev , alpha = c(0.3,0.6,0.9), draw= FALSE,smooth=1,color=heat.colors(n=length(lev)))
     drawScene.rgl(list(blob2,blob,blob1,brain))
-    } else drawScene.rgl(list(blob,blob1,brain)) # surface render 
+    return ( list(blob2,blob,blob1,brain) )
+    }
+  drawScene.rgl(list(blob,blob1,brain)) # surface render 
 # movie3d(spin3d(),duration=10)
+  return( list(blob,blob1,brain) ) 
 }
+
+
+
+
+#Make a function that will make each facet from data returned from surfaceTriangles applied to a function
+#(probably a more elegant way to do this?)
+makefacet<-function(data){
+  #Code for 3D function->stl files for molding and casting
+#stl creation functions similar to week 4 files
+#Laura Perovich Oct 2012
+#Load package misc3d that includes surfaceTriangles function
+#Define character constants used in the stl files
+tristart1<-"facet normal 0 0 0"
+tristart2<-" outer loop"
+triend1<-" endloop"
+triend2<-"endfacet"
+startline1<-"+"
+startline2<-" solid LAURA"
+endline<-" endsolid LAURA"
+
+facetvector<-c()
+progress <- txtProgressBar( min = 0, max = nrow(data[[1]]), style = 3 )
+for (i in 1:nrow(data[[1]])){
+v1<-paste("  vertex", as.character(data[[1]][i, 1]), as.character(data[[1]][i, 2]), as.character(data[[1]][i, 3]), sep=" ")
+v2<-paste("  vertex", as.character(data[[2]][i, 1]), as.character(data[[2]][i, 2]), as.character(data[[2]][i, 3]), sep=" ")
+v3<-paste("  vertex", as.character(data[[3]][i, 1]), as.character(data[[3]][i, 2]), as.character(data[[3]][i, 3]), sep=" ")
+facetvector<-c(facetvector, tristart1, tristart2, v1, v2, v3, triend1, triend2) 
+  if( i %% 50 == 0 )
+    {
+    setTxtProgressBar( progress, i )
+    }
+}
+return(facetvector)
+}
+
+#Make a function that puts the facets together with the file headers and writes it out
+makestl<-function( facetvector, outfile ){
+  #Code for 3D function->stl files for molding and casting
+#stl creation functions similar to week 4 files
+#Laura Perovich Oct 2012
+#Load package misc3d that includes surfaceTriangles function
+library(misc3d)
+#Define character constants used in the stl files
+tristart1<-"facet normal 0 0 0"
+tristart2<-" outer loop"
+triend1<-" endloop"
+triend2<-"endfacet"
+startline1<-"+"
+startline2<-" solid LAURA"
+endline<-" endsolid LAURA"
+fileConn<-file(outfile)
+myout<-c(startline1, startline2, facetvector, endline)
+writeLines( myout, fileConn)
+close(fileConn)
+}
+############################ to use this do ############################
+# library(ANTsR)
+# source('R/renderSurfaceFunction.R')
+# fn<-"/Users/stnava/Downloads/resimplerenderingexample/wmss.nii.gz"
+# img<-antsImageRead(fn,3)
+# brain<-renderSurfaceFunction( img )
+# fv<-makefacet(brain[[1]])
+# makestl(fv,"/tmp/temp.stl")
+
+getvertices<-function( inrglmesh )
+  {
+  cter<-nrow(inrglmesh[[1]])
+  vertices <- matrix( NA, nrow=3 * cter, ncol=3)
+  inds<-c(1:cter)
+  vertices[ ( 3* inds - 2 ) , ]<-inrglmesh[[1]]
+  vertices[ ( 3* inds - 1 ) , ]<-inrglmesh[[2]]
+  vertices[ ( 3* inds - 0 ) , ]<-inrglmesh[[3]]
+  indices<-rep(NA, nrow(vertices) )
+  return( list( vertices= vertices , indices= indices ) )
+  }
+# vtri <- surfaceTriangles(vertices[,1], vertices[,2], vertices[,3] , color="red")
+# drawScene(updateTriangles(vtri, material = "default", smooth = 3) )
