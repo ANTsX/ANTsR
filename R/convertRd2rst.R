af787d1e365caf161f9d71c781b1bc9923a34807
brian avants <stnava@gmail.com>
2013-02-25 18:57:41 -0500

    WIP: better website Rd 2 rst conversion

--- R/convertRd2rst.R
+++ R/convertRd2rst.R
@@ -11,8 +11,8 @@ convertRd2rst<- function( infile , outfile="")
   rstheader<-"=========================\n"
   Rd <- parse_Rd(infile)
   tags <- tools:::RdTags(Rd)
-  myfn<-Rd[[which(tags == "\\name")]][[1]]
-  myfn<-paste(myfn,"\n")
+  myfnr<-Rd[[which(tags == "\\name")]][[1]]
+  myfn<-paste(myfnr,"\n")
   cat(rstheader,file=outfile,append = FALSE)
   cat(myfn,file=outfile,append = TRUE)
   cat(rstheader,file=outfile,append = TRUE)
@@ -44,16 +44,19 @@ convertRd2rst<- function( infile , outfile="")
   cat(myusage,file=outfile,append = TRUE)
 
   baseexamples<-Rd[[which(tags == "\\examples")]]
+  myrrsthd<-paste(".. {r ",myfnr,",eval=FALSE,warning=FALSE,results=\"hide\",message=FALSE,echo=TRUE,results=\"hide\"} \n ")
+  cat(myrrsthd,file=outfile,append = TRUE)
+ 
   dlength<-length( baseexamples[[2]] )
   myexamples<-baseexamples[[2]][[1]][1]
   if ( dlength > 1 ) {
   for ( i in c(2:(dlength)) ) {
     myexamples<-paste(myexamples,"\n",baseexamples[[2]][[i]][1])
   }
-  }
+  } 
   myexamples<-paste("examples: ","\n",myexamples,"\n")
   cat(myexamples,file=outfile,append = TRUE)
-
+  cat(".. ..\n",file=outfile,append = TRUE)
   # FIXME - need to parse arguments and loop over all 
 #  myarguments<-Rd[[which(tags == "\\arguments")]][[2]][1]
 #  myarguments<-paste("arguments: ",myarguments)
