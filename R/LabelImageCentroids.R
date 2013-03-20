24473ec52e9468660cf6fd3c38556b6ed5e42feb
brian avants <stnava@gmail.com>
2013-03-20 13:54:50 -0400

    ENH: allow non-continuous labels + add progress bar to LabelImageCentroids

--- R/LabelImageCentroids.R
+++ R/LabelImageCentroids.R
@@ -3,6 +3,11 @@ LabelImageCentroids <- function( img, Physical=FALSE )
 
   # Get centroids of labels
   d <- dim(img)
+  if ( length(d) != 3 )
+    {
+    print("Currently only works for 3D")
+    return(1)
+    }
   xcoords <- rep(c(1:d[1]), d[2]*d[3] )
   ycoords <- rep(c(1:d[2]), each=d[1], d[3] )
   zcoords <- rep(c(1:d[3]), each=(d[1]*d[2]) )
@@ -13,13 +18,18 @@ LabelImageCentroids <- function( img, Physical=FALSE )
   xc <- rep(0.0,nLabels)
   yc <- rep(0.0,nLabels)
   zc <- rep(0.0,nLabels)
-
-  for ( i in c(1:nLabels) )
+  mylabels<-unique( labels[ labels > 0 ] )
+  progress <- txtProgressBar( min = 0, max = length(mylabels), style = 3 )
+  for ( i in mylabels )
     {
     idx <- (labels == i)
     xc[i] <- mean( subset(xcoords, idx) )
     yc[i] <- mean( subset(ycoords, idx) )
     zc[i] <- mean( subset(zcoords, idx) )
+    if( i %% 50 == 0 )
+      {
+      setTxtProgressBar( progress, i )
+      }
     }
   centroids <- cbind(xc,yc,zc)  
 
