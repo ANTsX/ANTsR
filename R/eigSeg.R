f1fdd5df97c3857074ced420bcf558c05b0fa7e8
brian avants <stnava@gmail.com>
2013-04-05 10:37:29 -0400

    WIP: toward tal coords - eigenanatomy segmentation - imageListToMatrix

--- R/eigSeg.R
+++ R/eigSeg.R
@@ -1,6 +1,6 @@
 eigSeg <- function( mask = NA, imgList = NA )
   {
-  if ( typeof(mask) != "S4" | typeof(imgList) != "list" )
+  if ( typeof(mask) != "S4"  )
     {
     print( args( eigSeg ) )
     return( 1 )
@@ -10,7 +10,10 @@ eigSeg <- function( mask = NA, imgList = NA )
   maskseg[ maskvox ]<-0
   if ( length( imgList ) > 0 )
     {
-    mydata <- imageListToMatrix( imgList, mask )
+    if ( typeof(imgList) == "list" )
+      mydata <- imageListToMatrix( imgList, mask )
+    if ( typeof(imgList) == "character" )
+      mydata <- imagesToMatrix( imgList, mask )
     segids<-apply( abs( mydata ) , 2 , which.max)
     segmax<-apply( abs( mydata ) , 2 , max)
     maskseg[ maskvox ]<-( segids * ( segmax > 0 ) )
