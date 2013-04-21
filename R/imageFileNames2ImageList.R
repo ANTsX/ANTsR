914c96cca3282378268ce57c933ec31b65c9d1fb
brian avants <stnava@gmail.com>
2013-04-21 17:04:55 -0400

    ENH: simple tool for converting fn to image list

---
+++ R/imageFileNames2ImageList.R
@@ -0,0 +1,16 @@
+imageFileNames2ImageList <- function( x, dim )
+  {
+  if ( nargs() == 0 )
+    {
+    print("Usage:  ilist<-imageFileNames2ImageList( x , imageDimension ) ")
+    return(1)
+    }
+  ilist<-list()
+  for ( i in x ) 
+    {
+    ilist<-lappend( ilist, antsImageRead( i , dim ) )
+    }
+  return( ilist )
+  }
+
+
