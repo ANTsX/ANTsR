4def6c58f326ef0507849cfe688fd54dc961bc25
brian avants <stnava@gmail.com>
2013-04-22 13:36:50 -0400

    ENH: extending list append functionality to allow prepend as well as append & joining of two lists

--- R/lappend.R
+++ R/lappend.R
@@ -1,6 +1,27 @@
 lappend <- function(lst, obj) {
+  if ( typeof(list) == "list"  & typeof( obj ) != "list" )
+    {
     lst[[length(lst)+1]] <- obj
     return(lst)
+    }
+  if ( typeof(obj) == "list" & typeof( lst ) != "list" )
+    {
+    lstout<-list( lst )
+    for ( i in 1:length(obj) )
+      {
+      lstout[[length(lstout)+1]] <- obj[[i]]
+      }
+    return(lstout)
+    }
+  if ( typeof(obj) == "list" & typeof( lst ) == "list" )
+    {
+    for ( i in 1:length(obj) )
+      {
+      lst[[length(lst)+1]] <- obj[[i]]
+      }
+    return(lst)
+    }
+  else return(NA)
 }
 
 
