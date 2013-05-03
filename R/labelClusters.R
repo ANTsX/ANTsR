5a9b9fe34ecd19b5093ddd7424627f3d78ad855d
brian avants <stnava@gmail.com>
2013-05-03 16:29:57 -0400

    BUG: labelClusters was failing; DOC: getMultivariateTemplateCoordinates.R

--- R/labelClusters.R
+++ R/labelClusters.R
@@ -1,14 +1,14 @@
-labelClusters <- function( x , minClusterSize = 50 , minThresh = 1.e-6 , maxThresh = 1 )
+labelClusters <- function( imagein , minClusterSize = 50 , minThresh = 1.e-6 , maxThresh = 1 )
   {
   if ( nargs() == 0 )
     {
-    print("Usage: clusers<-labelClusters( x , minClusterSize = 50 ) ")
-    print(" x is an antsImage ")
+    print("Usage: clusers<-labelClusters( imagein , minClusterSize = 50 ) ")
+    print(" imagein is an antsImage ")
     return(1)
     }
-  dim<-x@dimension
-  clust<-antsImageClone( x )
-  ThresholdImage(dim,x,clust, minThresh, maxThresh)
+  dim<-imagein@dimension
+  clust<-antsImageClone( imagein )
+  ThresholdImage(dim,imagein,clust, minThresh, maxThresh)
   LabelClustersUniquely(dim,clust,clust,minClusterSize)
   labs<-unique( clust[ clust > 0 ] )
   for ( i in 1:length(labs) )
