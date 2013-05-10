0b24d10034c15e6bafe92a686ccbe2c10b7101ae
Jeffrey Duda <jeff.duda@gmail.com>
2013-05-10 16:43:50 -0400

    BUG: updated usage to reflect need for list input

--- R/renderSurfaceFunction.R
+++ R/renderSurfaceFunction.R
@@ -2,7 +2,7 @@ renderSurfaceFunction<-function( surfimg, funcimg, surfval=0.5, basefval , offse
   {
   if ( missing(surfimg) )
     {
-    stop('Check usage:  at minimum, you need to call \n renderSurfaceFunction( an_ants_image ) \n ')
+    stop('Check usage:  at minimum, you need to call \n renderSurfaceFunction( list(an_ants_image) ) \n ')
     }
   smoothsval <- rep(smoothsval, length.out=length(surfimg))
   for (i in 1:length(surfimg)){
