a694b199dfcf209591e9b64c8261a4ce57ac8bc7
Ben_Kandel <bkandelemail-github@yahoo.com>
2013-06-04 16:30:13 -0400

    BUG: MeasureMinMaxMean didn't work.

--- R/MeasureMinMaxMean.R
+++ R/MeasureMinMaxMean.R
@@ -1,3 +1,8 @@
-MeasureMinMaxMean <- function(...){
-	.Call( "MeasureMinMaxMean", as.character( c(...) ) ) ;
+MeasureMinMaxMean <- function( image ){
+  if( missing(image))
+    stop("Missing input image.")
+  img.min <- min(as.array(image)) 
+  img.mean <- mean(as.array(image))
+  img.max <- max(as.array(image))
+  list(img.min=img.min, img.max=img.max, img.mean=img.mean)
 }
