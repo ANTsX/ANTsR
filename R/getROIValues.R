a07ff5ab7d9d2e15146e5e8a26118a81b880612a
brian avants <stnava@gmail.com>
2013-06-06 17:43:00 -0400

    BUG: uniqued the wrong image

--- R/getROIValues.R
+++ R/getROIValues.R
@@ -5,12 +5,15 @@ getROIValues <- function( valueImage, roiImage, maskImage )
     print( args( getROIValues ) ) 
     return(1)
     }
-  uvals<-sort( unique( valueImage[ maskImage == 1 ] ) )
+  uvals<-sort( unique( roiImage[ maskImage == 1 ] ) )
   roivals<-rep(NA,length(uvals))
   tempimg <- antsImageClone( roiImage )
   ImageMath(3,tempimg,'m',roiImage,maskImage)
+  print(length(uvals))
+  pb <- txtProgressBar( min = 0, max = length(uvals), style = 3 )
   for ( x in 1:length(uvals) )
     {
+    setTxtProgressBar( pb , x )
     inds<-( tempimg == uvals[x]   )
     if ( sum( inds ) > 0 ) roivals[x]<-mean( valueImage[ inds ] )
     }
