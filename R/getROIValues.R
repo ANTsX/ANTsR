95041eac9a4b5fc1f04fb6245ec27aef6d6ea68e
brian avants <stnava@gmail.com>
2013-06-06 17:57:04 -0400

    BUG: not indexing correctly

--- R/getROIValues.R
+++ R/getROIValues.R
@@ -7,14 +7,11 @@ getROIValues <- function( valueImage, roiImage, maskImage )
     }
   uvals<-sort( unique( roiImage[ maskImage == 1 ] ) )
   roivals<-rep(NA,length(uvals))
-  tempimg <- antsImageClone( roiImage )
-  ImageMath(3,tempimg,'m',roiImage,maskImage)
-  print(length(uvals))
   pb <- txtProgressBar( min = 0, max = length(uvals), style = 3 )
   for ( x in 1:length(uvals) )
     {
     setTxtProgressBar( pb , x )
-    inds<-( tempimg == uvals[x]   )
+    inds<-( ( roiImage == uvals[x] ) & ( maskImage == 1 )   )
     if ( sum( inds ) > 0 ) roivals[x]<-mean( valueImage[ inds ] )
     }
   return( list( roiValues = uvals,  roiMeans = roivals ) )
