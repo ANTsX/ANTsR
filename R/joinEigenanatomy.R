389490ff6fe0602a16385e46daf4ffde50ee0239
brian avants <stnava@gmail.com>
2013-08-05 14:46:47 -0400

    ENH: joinEigenanatomy.R + doc + onLoad

---
+++ R/joinEigenanatomy.R
@@ -0,0 +1,34 @@
+joinEigenanatomy <- function( datamatrix, mask, list_of_eanat_images , graphdensity = 0.65  )
+  {
+  if ( nargs() == 0 )
+    {
+    print("Usage:  x_joined<-joinEigenanatomy( datamatrix, list_of_eanat_images , gdensity = 0.65  ) ")
+    return(1)
+    }
+  pckg = try(require(igraph))
+  if(!pckg) {
+    getPckg("igraph")
+  }
+  library(igraph)
+  decom2<-imageListToMatrix( list_of_eanat_images, mask)
+  myproj<-datamatrix %*% t( decom2 )
+  gg<-makeGraph( cor( myproj ), graphdensity ) 
+  communitymembership<-gg$walktrapcomm$membership
+  newelist<-list()
+  for ( cl in 1:max( communitymembership ) )
+    {
+      newe<-antsImageClone( mydecom$eigenanatomyimages[[1]] )
+      newe[ mask > 0 ]<-0 
+      templist<-mydecom$eigenanatomyimages[ communitymembership == cl   ]
+      for ( eimg in templist ) {   
+        newe[ mask > 0 ] <- newe[ mask > 0 ] + eimg[ mask > 0 ] / sum( eimg[ mask > 0 ] ) 
+        print(  sum( newe > 0 )/ sum( mask > 0 ) )
+      }
+      newe[ mask > 0 ] <- newe[ mask > 0 ] / sum( newe[ mask > 0 ] )
+      newelist<-lappend( newelist, newe )
+    }
+  decom2<-imageListToMatrix( newelist, mask)
+  myproj<-datamatrix %*% t( decom2 )
+  colnames(myproj)<-paste("V",1:ncol(myproj),sep='')
+  return( list( fusedlist=newelist, fusedproj = myproj, memberships = communitymembership ) )
+}
---
+++ R/onLoad.R
@@ -0,0 +1,6 @@
+
+.onLoad <- function(libname, pkgname){
+#.libPaths( c( .libPaths(), "/Users/stnava/code/gitANTs/ANTsR/src/ANTS/ANTS-build/lib/" ) ) 
+print("Welcome to ANTsR")
+}
+
