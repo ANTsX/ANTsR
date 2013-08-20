d7836e0797df3ff8a4f7b0ba9a3e2b3cf115cd56
Jeffrey Duda <jeff.duda@gmail.com>
2013-08-20 14:43:45 -0400

    ADD: slice timing correction for BOLD fMRI

---
+++ R/sliceTimingCorrection.R
@@ -0,0 +1,15 @@
+sliceTimingCorrection <- function( fmri, sliceTime=NA )
+
+{
+tr <- antsGetSpacing(fmri)[length(dim(fmri))]
+
+if ( is.na(sliceTime) ) 
+{
+  sliceTime <- tr / ( dim(fmri)[length(dim(fmri))-1]+1 )
+  print( paste( "Assuming a slice timing of", sliceTime ) )
+}
+
+corrected <- antsImageClone(fmri)
+ImageMath( 4, corrected, "SliceTimingCorrection", fmri, sliceTime, "sinc" )
+return( corrected )
+}
