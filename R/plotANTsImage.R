e1a9300efdf0e70b0ca4324c63835a1c9008d653
brian avants <stnava@gmail.com>
2013-09-12 17:43:31 -0400

    ENH: make window right size in plotANTsImage

--- R/plotANTsImage.R
+++ R/plotANTsImage.R
@@ -205,7 +205,9 @@ for ( sl in c(0:(length(slices)-1)) ) {
 # onm<-paste(output,'.jpg',sep='')
 mag<-quality
 if ( !is.na(outname) ) jpeg(outname,width = ncol(bigslice)*mag, height = nrow(bigslice)*mag, units = "px",quality=75,bg="white")
-x <- pixmapGrey(bigslice, nrow=nrow(bigslice) )
+pixperinch<-96
+dev.new(height=nrow(bigslice)/pixperinch, width=ncol(bigslice)/pixperinch )
+x <- pixmapGrey(bigslice, nrow=nrow(bigslice), ncol=ncol(bigslice) )
 # dd<-pixmapRGB(c(bigslice,bigslice,bigslice),nrow=nrow(bigslice),ncol=ncol(bigslice),bbox=c(0,0,wincols,winrows))
 # plot(dd)
 par(mar=c(0,0,0,0) + 0.0)   # set margins to zero ! less wasted space
