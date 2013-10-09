27c4ede9886cdae4dcecdd2822a40f6be9db6077
brian avants <stnava@gmail.com>
2013-10-09 11:37:35 -0400

    ENH: taskFMRI function - AR(2) + formula option

--- R/taskFMRI.R
+++ R/taskFMRI.R
@@ -1,4 +1,5 @@
-taskFMRI <- function( mat , hrf, myvars )
+taskFMRI <- function( mat , hrf, myvars , residualizedesignmatrix = FALSE, myformula="globalsignal + motion1 +   
+      motion2 + motion3 + compcorr1 + compcorr2 + compcorr3"  )
   {
   if ( nargs() == 0 )
     {
@@ -9,49 +10,55 @@ taskFMRI <- function( mat , hrf, myvars )
   mask<-myvars$mask 
   nuis<-( myvars$nuisancevariables )
   betas<-rep(NA, ncol( mat ) )
-  desmat<-cbind( myvars$globalsignal, nuis[,2:7] )
-#  desmat<-residuals( lm( desmat ~ hrf ) )
+  desmat<-cbind( myvars$globalsignal, nuis )
+  if ( residualizedesignmatrix ) desmat<-residuals(lm(desmat~hrf))
   desmat<-cbind( hrf, desmat )
-  colnames( desmat )<-c("hrf","globsig",colnames(nuis)[2:7])
-  residmat<-residuals( lm( mat ~  hrf + globsig + motion1 +   
-      motion2 + motion3 + compcorr1 + compcorr2 + compcorr3 , 
-      data = data.frame( desmat )  )  )
-
+  colnames( desmat )<-c("hrf","globalsignal",colnames(nuis))
+  print( colnames( desmat ) )
 ############################################
 # Statistical methods of estimation and inference for  #
 # functional MR image analysis Bullmore 1996            #
 ############################################
+  amat <- mat
+  adesmat <- desmat
+  myformhrf<-as.formula( paste( "amat ~  hrf + ", myformula ) )
+  myformNOhrf<-as.formula( paste( "amat ~ 1 + ", myformula ) )
+  print( myformhrf )
+  for ( i in 1:5 ) {
+  residmat<-residuals( lm( myformhrf , 
+      data = data.frame( adesmat )  )  )
   residsig <- apply( residmat, FUN=mean, MARGIN=1 ) 
   arcoefs<-ar( residsig, FALSE, 2 )$ar  # use something similar to  SPM's  autoregression estimate
   print( arcoefs )
-  mat1 <- ashift(mat,c(1,0))
-  mat1[1,] <- mat[1, ]
-  mat2 <- ashift(mat,c(2,0))
-  mat2[1,] <- mat[1, ]
-  mat2[2,] <- mat[2, ]
-  amat <- mat - mat1 * arcoefs[1]  - mat2 * arcoefs[2] 
-  mat1 <- ashift(desmat,c(1,0))
-  mat1[1,] <- desmat[1, ]
-  mat2 <- ashift(desmat,c(2,0))
-  mat2[1,] <- desmat[1, ]
-  mat2[2,] <- desmat[2, ]
-  adesmat <- desmat - mat1 * arcoefs[1]  - mat2 *  arcoefs[2] 
+  mat1 <- ashift(amat,c(1,0))
+  mat1[1,] <- amat[1, ]
+  mat2 <- ashift(amat,c(2,0))
+  mat2[1,] <- amat[1, ]
+  mat2[2,] <- amat[2, ]
+  amat <- amat - mat1 * arcoefs[1]  - mat2 * arcoefs[2] 
+  mat1 <- ashift(adesmat,c(1,0))
+  mat1[1,] <- adesmat[1, ]
+  mat2 <- ashift(adesmat,c(2,0))
+  mat2[1,] <- adesmat[1, ]
+  mat2[2,] <- adesmat[2, ]
+  adesmat <- adesmat - mat1 * arcoefs[1]  - mat2 *  arcoefs[2]
+  }
   #
   # new regression 
-  amat<-residuals( lm( amat ~ globsig + motion1 + motion2 + motion3 + compcorr1
-    + compcorr2 + compcorr3, data = data.frame( adesmat )  ) )
+  amat<-residuals( lm( myformNOhrf, data = data.frame( adesmat )  ) )
+  residsig <- apply( amat, FUN=mean, MARGIN=1 ) 
+  arcoefs<-ar( residsig, FALSE, 2 )$ar  # use something similar to  SPM's  autoregression estimate
+  print(paste("final arcoefs", arcoefs[1], arcoefs[2] ) )
   progress <- txtProgressBar(min = 0, max = ncol(mat),      style = 3)
   for ( i in 1:ncol(amat) ) {
     vox<-amat[ , i ]
 #    mdl<-lmrob( vox ~  hrf, data = data.frame( adesmat )  )
-    mdl<-lm( vox ~  hrf , data = data.frame( desmat )  ) 
+    mdl<-lm( vox ~  hrf , data = data.frame( adesmat )  ) 
     betas[i]<-coefficients(summary(mdl))[2,3] # probably better way
 #    betas[i]<-cor.test( vox , hrf )$est  
     setTxtProgressBar(progress, i)
     }
   close(progress)
   betas[ is.na( betas ) ] <- 0
-  return( betas ) 
+  return( list( beta = betas , fmrimat=amat ) )
   }
-
-
