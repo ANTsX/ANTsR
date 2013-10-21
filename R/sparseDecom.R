a35f52634a7435f9419a0b48185c394aa0f533bd
brian avants <stnava@gmail.com>
2013-10-21 14:12:55 -0400

    BUG: antsApplyTransforms compositetransformtype; ENH: mycoption in sparsedecom

--- R/sparseDecom.R
+++ R/sparseDecom.R
@@ -1,4 +1,4 @@
-sparseDecom <- function( inmatrix=NA,  inmask=NA , sparseness=0.01, nvecs=50 , its=5 , cthresh=250 , statdir = NA, z=0 , smooth=0, initializationList = list() )
+sparseDecom <- function( inmatrix=NA,  inmask=NA , sparseness=0.01, nvecs=50 , its=5 , cthresh=250 , statdir = NA, z=0 , smooth=0, initializationList = list() , mycoption=1)
 {
   numargs<-nargs()
   if ( numargs < 1 | missing(inmatrix) )
@@ -17,7 +17,7 @@ sparseDecom <- function( inmatrix=NA,  inmask=NA , sparseness=0.01, nvecs=50 , i
     mfn<-paste(statdir,'spcamask.nii.gz',sep='')
     antsImageWrite( inmask, mfn )
     } 
-  args<-list("--svd",paste("recon[",matname,",",mfn,",",sparseness,"]",sep=''),"--l1",1,"-i",its,"--PClusterThresh",cthresh,"-n",nvecs,"-o",outfn,"-z",z,"-s",smooth,"-c",1,"--mask",inmask)
+  args<-list("--svd",paste("recon[",matname,",",mfn,",",sparseness,"]",sep=''),"--l1",1,"-i",its,"--PClusterThresh",cthresh,"-n",nvecs,"-o",outfn,"-z",z,"-s",smooth,"-c",mycoption,"--mask",inmask)
   if ( length(initializationList) > 0 )
     {
     ct<-1
@@ -33,7 +33,7 @@ sparseDecom <- function( inmatrix=NA,  inmask=NA , sparseness=0.01, nvecs=50 , i
     fileConn<-file(initlistfn)
     writeLines(outfns, fileConn)
     close(fileConn)
-    args<-list("--svd",paste("recon[",matname,",",mfn,",",sparseness,"]",sep=''),"--l1",1,"-i",its,"--PClusterThresh",cthresh,"-n",nvecs,"-o",outfn,"-z",z,"-s",smooth,"-c",1,"--mask",mfn,"--initialization",initlistfn)
+    args<-list("--svd",paste("recon[",matname,",",mfn,",",sparseness,"]",sep=''),"--l1",1,"-i",its,"--PClusterThresh",cthresh,"-n",nvecs,"-o",outfn,"-z",z,"-s",smooth,"-c",mycoption,"--mask",mfn,"--initialization",initlistfn)
     print( initlistfn )
     }
   .Call( "sccan", int_antsProcessArguments( c(args) ) , PACKAGE = "libRsccan" ) ;
