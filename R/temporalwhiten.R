79ae5203b7f7da77a3ab576edb06a8a270a24c40
brian avants <stnava@gmail.com>
2013-10-24 12:45:25 -0400

    ENH: temporal whitening

---
+++ R/temporalwhiten.R
@@ -0,0 +1,18 @@
+temporalwhiten <- function( x, myord=2 )
+  {
+  if ( nargs() == 0 )
+    {
+    print("Usage:  x_whitened<-whiten( x ) ")
+    return(1)
+    }
+  omat<-mat
+  for ( i in 1:ncol(mat) )
+    {
+    gsig<-mat[,i]
+    arval<-ar(gsig,FALSE,2)$ar
+    omat[,i]<-shift(gsig,1)*arval[1]+shift(gsig,2)*arval[2]
+    }
+  return( omat )
+  }
+
+
