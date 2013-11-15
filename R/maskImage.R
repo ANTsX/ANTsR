b3834e1eedfd2971702bf7b0ac7286aa28833bf6
brian avants <stnava@gmail.com>
2013-11-15 11:45:43 -0500

    ENH: maskImage now takes a list option

--- R/maskImage.R
+++ R/maskImage.R
@@ -1,5 +1,21 @@
-maskImage <- function(img.in, img.mask, level = 1) {
-  img.out <- antsImageClone(img.in)
-  img.out[img.mask != level] <- 0
-  return(img.out)
-} 
+maskImage <- function(img.in,img.mask, level=1, binarize = FALSE ) {
+  if ( class(level) == "numeric" )
+    {
+      img.out <- antsImageClone(img.in)
+      img.out[ img.mask != level ] <- 0
+      return(img.out)
+    }
+  if ( class(level) == "list" )
+    {
+      img.out <- antsImageClone(img.in)
+      img.out[ img.out > 0 ] <- 0
+      for ( mylevel in level )
+        {
+          myval<-as.numeric( mylevel )
+          if (  binarize ) img.out[ img.mask == myval ] <- 1
+          if ( !binarize ) img.out[ img.mask == myval ] <- img.in[ img.mask == myval ]
+        }
+      return(img.out)
+    }
+  
+}
