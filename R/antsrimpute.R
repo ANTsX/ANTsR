c4673a973324024378a151ed59afdb0335ede223
BBAvants <stnava@gmail.com>
2014-02-03 13:21:55 -0500

    ENH: antsr impute for vector

--- R/antsrimpute.R
+++ R/antsrimpute.R
@@ -1,4 +1,12 @@
 antsrimpute <-function( mydat, FUN=mean, ...) {
-  for ( x in 1:ncol(mydat) ) mydat[is.na(mydat[,x]),x]<- FUN(as.numeric(mydat[,x]), na.rm=T, ...)
-  mydat
+  if ( is.null( dim( mydat ) ) )
+      {
+        mydat2<-mydat
+        mydat2[ is.na(mydat) ]<- FUN(as.numeric(mydat), na.rm=T, ...)
+        mydat2
+      } else {
+          mydat2<-mydat
+          for ( x in 1:ncol(mydat) ) mydat2[is.na(mydat[,x]),x]<- FUN(as.numeric(mydat[,x]), na.rm=T, ...)
+          mydat2
+      }
 }
