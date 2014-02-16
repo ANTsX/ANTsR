7a8e484faa6d2aaf159ed2ca4308b4563051ebd0
BBAvants <stnava@gmail.com>
2014-02-16 13:09:06 -0500

    ENH: add edge weights

--- R/plotBasicNetwork.R
+++ R/plotBasicNetwork.R
@@ -1,4 +1,5 @@
-plotBasicNetwork <- function(centroids, brain) {
+plotBasicNetwork <- function(centroids, brain, weights = NA, edgecolors = 0, nodecolors = "blue", 
+  nodetype = "s" , scaling = c(0, 0), lwd = 2, radius = 3 ) {
   if (missing(centroids) | missing(brain)) {
     print(args(plotBasicNetwork))
     return(1)
@@ -10,13 +11,41 @@ plotBasicNetwork <- function(centroids, brain) {
   nSurfaceVerts <- dim(mesh$vertices)[1]
   mesh$vertices <- rbind(mesh$vertices, as.matrix(centroids))
   labelVerts <- c(1:nrow(centroids)) + nSurfaceVerts
-  spheres3d(mesh$vertices[labelVerts, ], col = "blue", type = "s", radius = 3)
+  spheres3d(mesh$vertices[labelVerts, ], color = nodecolors, type = nodetype, radius = radius)  
   edgelocations <- c()
+  edgeweights <- c()
   for (i in c(1:nLabels)) {
     for (j in c(i:nLabels)) {
-      edgelocations <- c(edgelocations, nSurfaceVerts + c(i, j))
+      if ( is.na( weights ) ) edgelocations <- c(edgelocations, nSurfaceVerts + c(i, j))
+       else if ( weights[i,j] > 0 &  weights[i,j] < Inf ) {
+           edgelocations <- c(edgelocations, nSurfaceVerts + c(i, j))
+           edgeweights <- c(edgeweights,  weights[i,j])
+       }
     }
   }
-  segments3d(mesh$vertices[edgelocations, ], col = "red", lwd = 2)
+  if ( is.na(weights) ) {
+      segments3d(mesh$vertices[edgelocations, ], col = "red", lwd = 2)
+      return(1)
+  }
+
+  if ((length(edgecolors) == 1) && (edgecolors[1] == 0)) {
+    if ((scaling[1] == scaling[2])) {
+      scaling[1] <- min(edgeweights)
+      scaling[2] <- max(edgeweights) - min(edgeweights)
+    }
+    
+    edgeweights <- edgeweights - scaling[1]
+    edgeweights <- edgeweights/scaling[2]
+    edgeweights <- edgeweights * 0.75  # prevent 'wrapping' of colors
+    
+    edgeweights <- 1 + (edgeweights * 511)
+    # colormap <- topo.colors(512)
+    colormap <- rainbow(512)
+    edgecolors <- edgeweights
+    for (i in c(1:length(edgeweights))) {
+      edgecolors[i] <- colormap[floor(edgeweights[i])]
+    }
+  }
+  segments3d(mesh$vertices[edgelocations, ], col = rep(edgecolors, each = 2), lwd = lwd)
   return(1)
 } 
