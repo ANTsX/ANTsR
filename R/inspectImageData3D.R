28f406093edd6a62fb90dd47a9d4004a223d991a
BBAvants <stnava@gmail.com>
2014-02-27 12:42:07 -0500

    ENH: inspectImageData3D

--- R/inspectImageData3D.R
+++ R/inspectImageData3D.R
@@ -1,9 +1,36 @@
-inspectImageData3D <- function(x) {
+inspectImageData3D <- function( myfiles ) {
   if (nargs() == 0) {
-    print("Usage:  x_whitened<-whiten( x ) ")
+    print( args( inspectImageData3D ) )
     return(1)
   }
-  svdx <- svd(x %*% t(x))
-  dd <- (svdx$d)^(-1/2)
-  xw <- ((svdx$u %*% diag(dd)) %*% t(svdx$v)) %*% x
+  pckg <- try(require(moments))
+  if (!pckg) {
+    getPckg("moments")
+  }
+  library(moments)
+  pckg <- try(require(extremevalues))
+  if (!pckg) {
+    getPckg("extremevalues")
+  }
+  library(extremevalues)
+  measnames<-c("mean1","mean2","sd1","sd2","kurt1","kurt2")
+  nmeas<-length(measnames)
+  idim<-3
+  nfn<-length(myfiles)
+  mymat<-matrix( rep(0,nfn*nmeas) , nrow=nfn )
+  colnames(mymat)<-measnames
+  for ( i in 1:nfn )
+      {
+      img<-antsImageRead(  myfiles[i] ,  idim )
+      thresh<-mean( img[ img > min(img) ] ) 
+      mymat[i,1]<-thresh
+      mymat[i,2]<-mean( img[ img > thresh ] )
+      mymat[i,3]<-sd( img[ img > min(img) ] )
+      mymat[i,4]<-sd( img[ img > thresh ] )
+      mymat[i,5]<-kurtosis( img[ img > min(img) ]  )
+      mymat[i,6]<-kurtosis( img[ img > thresh ] )
+      }
+  rownames(mymat)<-myfiles
+  return( mymat )
 } 
+
