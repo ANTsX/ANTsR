851ad211afd6553ae797c6fc32461b39f548922c
BBAvants <stnava@gmail.com>
2014-03-28 08:43:40 -0400

    ENH: speedup binsnps

--- R/binarizeSNPs.R
+++ R/binarizeSNPs.R
@@ -7,20 +7,18 @@ binarizeSNPs <- function(snps) {
   binsnps <- (matrix(rep(NA, nrow(snps) * ncol(snps) * nrep), nrow = nrow(snps), ncol = ncol(snps) * nrep))
   binsnpsdf <- data.frame(matrix(rep(NA, nrow(snps) * ncol(snps) * nrep), nrow = nrow(snps), ncol = ncol(snps) * 
     nrep))
-  ct <- 1
-  for (x in 1:ncol(snps)) {
-    # binsnps[ snps[,x] == 0 , ct ]<-1 ; names(binsnpsdf)[ct]<-paste(names(snps)[x],'.0',sep='') binsnps[ snps[,x]
-    # != 0 , ct ]<-0 ; ct<-ct+1
-    binsnps[snps[, x] == 1, ct] <- 1
-    names(binsnpsdf)[ct] <- paste(names(snps)[x], ".1", sep = "")
-    binsnps[snps[, x] != 1, ct] <- 0
-    ct <- ct + 1
-    binsnps[snps[, x] == 2, ct] <- 1
-    names(binsnpsdf)[ct] <- paste(names(snps)[x], ".2", sep = "")
-    binsnps[snps[, x] != 2, ct] <- 0
-    ct <- ct + 1
-  }
+  inds1<-seq(1,(ncol(binsnps)),by=2)
+  inds2<-inds1+1
+  binsnps[,inds1]<-snps
+  binsnps[,inds2]<-snps
+  ww<-( binsnps[,inds1] == 2 )
+  binsnps[,inds1][ww] <-0
+  binsnps[,inds1][!ww]<-1
+  ww<-( binsnps[,inds2] == 1 )
+  binsnps[,inds2][ww] <-0
+  binsnps[,inds2][!ww]<-1
   osnps <- data.frame(binsnps)
-  names(osnps) <- names(binsnpsdf)
+  names(osnps)[inds1] <- paste(names(binsnpsdf),".1",sep='')
+  names(osnps)[inds2] <- paste(names(binsnpsdf),".2",sep='')
   return(osnps)
 } 
