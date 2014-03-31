9bf5f0a788d9a54795d64625162a6df7ed5ec666
BBAvants <stnava@gmail.com>
2014-03-31 10:16:54 -0400

    DOC: neteanat example

--- R/networkEiganat.R
+++ R/networkEiganat.R
@@ -1,4 +1,4 @@
-networkEiganat <- function(Xin, sparseness = c(0.1, 0.1), nvecs = 5, its = 100, gradparam = 1, mask = NA, v, prior, pgradparam = 0.01, clustval=0, downsample=0, doscale=T, domin=T, verbose=F, dowhite=0, timeme=T ) {
+networkEiganat <- function(Xin, sparseness = c(0.1, 0.1), nvecs = 5, its = 20, gradparam = 1, mask = NA, v, prior, pgradparam = 0.01, clustval=0, downsample=0, doscale=T, domin=T, verbose=F, dowhite=0, timeme=T ) {
   X <- Xin
   if ( dowhite  > 0  &  ( nvecs*2 < nrow(Xin) ) ) X<-icawhiten( X, dowhite )
   if ( downsample > 0 &  ( nvecs < nrow(Xin) )  ) X<-lowrankRowMatrix( X, downsample )
