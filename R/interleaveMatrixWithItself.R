257f36acf24131ea69ee5ad8103d20fe90aff84b
BBAvants <stnava@gmail.com>
2014-04-17 01:59:05 -0400

    ENH: name the matrix

--- R/interleaveMatrixWithItself.R
+++ R/interleaveMatrixWithItself.R
@@ -6,11 +6,16 @@ interleaveMatrixWithItself <- function(x,n=1) {
   nc<-ncol(x)
   nr<-nrow(x)
   xi<-matrix( rep(0,nr*nc*n), nrow=nr )
+  colnames(xi)<-paste("V",1:ncol(xi))
+  colnamesxi<-colnames(xi)
+  colnamesx<-colnames(x)
   j<-1
   for ( i in 1:ncol(x) )
     {
     xi[,j:(j+n-1)]<-x[,i]
+    colnamesxi[j:(j+n-1)]<-paste(colnamesx[i],1:n,sep='')
     j<-j+n
     }
+  colnames(xi)<-colnamesxi
   return(xi)
 } 
