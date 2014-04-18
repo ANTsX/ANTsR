ec43a56eee3217b15742fe8c306536ca05874aa9
BBAvants <stnava@gmail.com>
2014-04-18 17:31:59 -0400

    BUG: antsImageClone seg fault if input image was not allocated

--- R/antsImageClone.R
+++ R/antsImageClone.R
@@ -1,3 +1,6 @@
 antsImageClone <- function(in_image, out_pixeltype = in_image@pixeltype) {
+   if( length(dim(in_image)) == 1 )
+       if( dim(in_image)[1] == 1 )
+           return(NULL)
   .Call("antsImageClone", in_image, out_pixeltype, PACKAGE = "ANTsR")
 } 
