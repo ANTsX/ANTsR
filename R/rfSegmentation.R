8b18ba1f5b3da2c07c26257bd45cdac2edd0f9bf
BBAvants <stnava@gmail.com>
2014-04-27 19:23:56 -0400

    WIP: rf segmentation

--- R/rfSegmentation.R
+++ R/rfSegmentation.R
@@ -1,14 +1,15 @@
-rfSegmentation <- function( labelimg, featureimages ) {
+rfSegmentation <- function( labelimg, featureimages, ntrees=100, verbose=FALSE ) {
   if (nargs() == 0) {
     print("Usage:  probs<-rfSegmentation( x, x2 ) ")
     return(1)
   }
   mask<-antsImageClone( labelimg )
   mask<-getMask( mask )
-  labels<-labelimg[ mask == 1 ]
+  labels<-as.factor( labelimg[ mask == 1 ] )
   fmat<-imageListToMatrix( featureimages , mask )
   mydf<-data.frame( labels = labels , fmat )
-  myrf<-randomForest( labels ~ . , data=mydf )
+  myrf<-randomForest( labels ~ . , data=mydf , ntree = ntrees , type = "classification", importance = TRUE, na.action = na.omit , do.trace=verbose )
+  if ( verbose ) print( myrf )
   # FIXME make probabilityimages
   probabilityimages<-0
   myout<-list( segs=predict(myrf), probabilityimages = probabilityimages )
