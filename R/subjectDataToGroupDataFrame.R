bdcda396bab9d2580aa08f803708f051eb5393af
BBAvants <stnava@gmail.com>
2014-05-21 16:08:45 -0400

    ENH: better use of subjectDataToGroupDataFrame.R + DOC

--- R/subjectDataToGroupDataFrame.R
+++ R/subjectDataToGroupDataFrame.R
@@ -1,17 +1,20 @@
-subjectDataToGroupDataFrame <- function( csvlist, usecol, mycolname=NA ) {
+subjectDataToGroupDataFrame <- function( csvlist, usecol, mycolname=NA, datarownames=NA ) {
   if (nargs() == 0) {
     print("Usage:  x_subjectDataToGroupDataFrameed<-subjectDataToGroupDataFrame( x, 1 ) ")
     return(1)
   }
   data1<-read.csv( csvlist[1] )
-  if ( is.na( mycolname ) ) mycolname<-colnames(data1)[usecol]
+  if ( ! is.numeric(usecol) ) mycol<-which( colnames(data1) == usecol ) else mycol<-usecol
+  if ( is.na(mycol) ) { print(paste("poorly chosen column name",mycol)); return; }
+  if ( is.na( mycolname ) ) mycolname<-colnames(data1)[mycol]
   ncl<-nrow(data1)
-  mycolnames<-paste(mycolname,rownames(ff),sep='')
+  if ( ! is.na( datarownames ) ) rownames(data1) <- datarownames
+  mycolnames<-paste(mycolname,rownames(data1),sep='')
   mat<-matrix( rep(NA,ncl*length(csvlist) ) , nrow=length(csvlist) )
   for ( i in 1:length(csvlist) )
     {
     data1<-read.csv( csvlist[i] )
-    mat[i,]<-data1[,usecol]
+    mat[i,]<-data1[,mycol]
     }
   mydf<-data.frame( mat )
   colnames( mydf )<-mycolnames
