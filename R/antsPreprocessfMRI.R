dff0529a09793fe1a8445948ae7e132aa38feec2
Nick Tustison <ntustison@gmail.com>
2014-05-28 18:11:45 -0400

    ENH:  Adding preprocessing function.

---
+++ R/antsPreprocessfMRI.R
@@ -0,0 +1,71 @@
+library( ANTsR )
+
+antsPreprocessfMRI <- function( boldImage,
+  maskImage = NA, maskingThreshold = 100,
+  initialNuisanceVariables = NA, doCompCor = TRUE,
+  doMotionCorrection = TRUE, useMotionCorrectedImage = FALSE,
+  spatialSmoothingParameter = 0.0, spatialSmoothingNumberOfIterations = 5,
+  frequencyLowThreshold = 0.01, frequencyHighThreshold = 0.1 )
+{
+
+# compute nuisance variables
+
+if( ! is.na( initialNuisanceVariables ) )
+  {
+  nuisanceVariables <- matrix( NA, nrow = 0, ncol = 0 )
+  } else {
+  nuisanceVariables <- initialNuisanceVariables
+  }
+
+if( doMotionCorrection )
+  {
+  motionCorrectionResults <- motion_correction( boldImage, moreaccurate = TRUE )
+  motionCorrectionParameters <- motionCorrectionResults$moco_params
+  nuisanceVariables <- as.matrix( motionCorrectionParameters )[,2:ncol( motionCorrectionParameters )]
+
+  if( useMotionCorrectedImage )
+    {
+    boldImage <- motionCorrectionResults$moco_img
+    }
+  }
+
+averageImage <- new( "antsImage", "float", 3 )
+antsMotionCorr( list( d = 3, a = boldImage, o = averageImage ) )
+
+if( is.na( maskImage ) )
+  {
+  maskImage <- getMask( averageImage, maskingThreshold, Inf, FALSE )
+  }
+averageImage[maskImage == 0] <- 0
+
+if( doCompCor )
+  {
+  compcorNuisanceVariables <- compcor( boldImage, maskImage, ncompcor = 6, variance_extreme = 0.975 )
+  if( dim( nuisanceVariables )[1] > 0 )
+    {
+    nuisanceVariables <- cbind( nuisanceVariables, compcorNuisanceVariables )
+    } else {
+    nuisanceVariables <- compcorNuisanceVariables
+    }
+  }
+
+# do nuisance regression then bandpass filtering
+# http://blogs.discovermagazine.com/neuroskeptic/2013/06/12/when-cleaning-fmri-data-is-a-nuisance/
+
+boldMatrix <- timeseries2matrix( boldImage, maskImage )
+boldResiduals <- residuals( lm( boldMatrix ~ 1 + nuisanceVariables ) )
+boldResidualsFiltered <- frequencyFilterfMRI( boldResiduals, tr = antsGetSpacing( boldImage )[4],
+                           freqLo = frequencyLowThreshold, freqHi = frequencyHighThreshold, opt = "trig" )
+
+cleanBoldImage <- matrix2timeseries( boldImage, maskImage, boldResidualsFiltered )
+
+# anisotropically smooth the images, if desired
+
+if( spatialSmoothingParameter > 0.0 & spatialSmoothingNumberOfIterations > 0 )
+  {
+  ImageMath( 4, cleanBoldImage, "PeronaMalik", cleanBoldImage,
+    spatialSmoothingParameter, spatialSmoothingNumberOfIterations )
+  }
+
+return( list( cleanBoldImage = cleanBoldImage, maskImage = maskImage ) )
+}
