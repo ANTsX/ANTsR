4188dd47f8c7376851caad3d17b396fbf4c3d1bc
BBAvants <stnava@gmail.com>
2014-06-10 13:13:26 -0400

    ENH: enable low rank whitening

--- R/whiten.R
+++ R/whiten.R
@@ -1,9 +1,23 @@
-whiten <- function(x) {
+whiten <- function( x , k = NA, reducex = FALSE ) {
   if (nargs() == 0) {
     print("Usage:  x_whitened<-whiten( x ) ")
     return(1)
   }
-  svdx <- svd(x %*% t(x))
-  dd <- (svdx$d)^(-1/2)
-  xw <- ((svdx$u %*% diag(dd)) %*% t(svdx$v)) %*% x
+  if ( is.na( k ) )
+    {
+    svdx <- svd(x %*% t(x))
+    dd <- (svdx$d)^(-1/2)
+    xw <- ((svdx$u %*% diag(dd)) %*% t(svdx$v)) %*% x
+    }
+  else
+    {
+    n<-nrow( x )
+    p<-ncol( x ) 
+    svdx <- svd( x %*% t(x) , nu = min( n , p , k ), nv = min( n , p , k ) )
+    dd <-diag( ( (svdx$d)^(-1/2) )[1:k] )
+    xw <- (svdx$u %*% dd ) %*% t(svdx$v)
+    xw <- ( xw ) %*% x 
+    }
+  if ( reducex )  xw <- lowrankRowMatrix( xw , k )
+  return( xw ) 
 } 
