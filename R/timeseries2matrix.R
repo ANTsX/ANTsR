f51336f92a8bfd113a67aac94f13391f63817f82
BBAvants <stnava@gmail.com>
2014-06-16 09:35:35 -0400

    PERF: speed for label timeseries2matrix

--- R/timeseries2matrix.R
+++ R/timeseries2matrix.R
@@ -1,26 +1,17 @@
 timeseries2matrix <- function(img, mask) {
   labs<-sort( unique( mask[ mask > 0.001 ] ) )
-  if ( length( labs ) == 1 )
+  if ( length( labs ) == 1 ) logmask <- (mask == 1) else logmask <- ( mask > 0 )
+  mat <- img[logmask]
+  dim(mat) <- c(sum(logmask), dim(img)[length(dim(img))])
+  mat<-t( mat )
+  if ( length( labs ) == 1 ) return( mat )
+  maskvec<-mask[ logmask ]
+  mmat<-matrix( apply(mat[ , maskvec == labs[1] ],FUN=mean,MARGIN=1) , ncol = 1 )
+  for ( i in 2:length(labs) )
     {
-    logmask <- (mask == 1)
-    mat <- img[logmask]
-    dim(mat) <- c(sum(logmask), dim(img)[length(dim(img))])
-    return(t(mat))
+    newmat<-matrix( apply(mat[ , maskvec == labs[i] ],FUN=mean,MARGIN=1) , ncol=1 )
+    mmat<-cbind( mmat,newmat)
     }
-  else { # return mean time series in each label
-    logmask <- (mask == labs[1] )
-    mat <- img[logmask]
-    dim(mat) <- c(sum(logmask), dim(img)[length(dim(img))])
-    mat<-matrix( apply(mat,FUN=mean,MARGIN=2) , ncol = 1 )
-    for ( i in 2:length(labs) )
-      {
-      logmask <- (mask == labs[i] )
-      newmat <- img[logmask]
-      dim(newmat) <- c(sum(logmask), dim(img)[length(dim(img))])
-      newmat<-matrix( apply(newmat,FUN=mean,MARGIN=2) , ncol=1 )
-      mat<-cbind(mat,newmat)
-      }
-    colnames(mat)<-paste("L",labs)
-    return( (mat) )
-  }
+  colnames( mmat )<-paste("L",labs)
+  return( mmat )
 } 
