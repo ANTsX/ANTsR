82f1b4455af0f4507a1816a8bc3fb26acc7b0ca5
BBAvants <stnava@gmail.com>
2014-06-15 23:25:26 -0400

    ENH: mean time series within labels

--- R/timeseries2matrix.R
+++ R/timeseries2matrix.R
@@ -1,7 +1,26 @@
 timeseries2matrix <- function(img, mask) {
-  logmask <- (mask == 1)
-  mat <- img[logmask]
-  dim(mat) <- c(sum(logmask), dim(img)[length(dim(img))])
-  
-  return(t(mat))
+  labs<-sort( unique( mask[ mask > 0.001 ] ) )
+  if ( length( labs ) == 1 )
+    {
+    logmask <- (mask == 1)
+    mat <- img[logmask]
+    dim(mat) <- c(sum(logmask), dim(img)[length(dim(img))])
+    return(t(mat))
+    }
+  else { # return mean time series in each label
+    logmask <- (mask == labs[1] )
+    mat <- img[logmask]
+    dim(mat) <- c(sum(logmask), dim(img)[length(dim(img))])
+    mat<-apply(mat,FUN=mean,MARGIN=1)
+    for ( i in 2:length(labs) )
+      {
+      logmask <- (mask == labs[i] )
+      newmat <- img[logmask]
+      dim(newmat) <- c(sum(logmask), dim(img)[length(dim(img))])
+      newmat<-apply(newmat,FUN=mean,MARGIN=1)
+      mat<-cbind(mat,newmat)
+      }
+    colnames(mat)<-paste("L",labs)
+    return( (mat) )
+  }
 } 
