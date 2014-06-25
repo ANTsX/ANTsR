df3cee9b975574dfa68712064094f1509e2784d9
BBAvants <stnava@gmail.com>
2014-06-25 10:30:19 -0400

    WIP: 4D sccan

--- R/makeImage.R
+++ R/makeImage.R
@@ -3,6 +3,7 @@ makeImage <- function( imagesize, voxval = 1 ) {
   outimg <- new("antsImage", "float", imagedimension)
   if ( imagedimension == 2 ) ImageMath(imagedimension,outimg,"MakeImage",imagesize[1],imagesize[2])
   if ( imagedimension == 3 ) ImageMath(imagedimension,outimg,"MakeImage",imagesize[1],imagesize[2],imagesize[3])
+  if ( imagedimension == 4 ) ImageMath(imagedimension,outimg,"MakeImage",imagesize[1],imagesize[2],imagesize[3],imagesize[4])
   outimg[ outimg == 0 ] <- voxval 
   return(outimg)
 }
