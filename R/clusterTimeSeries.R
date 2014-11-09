e28c617ab7d4f7b29ed4dd90ff81d2a32e58c36d
BBAvants <stnava@gmail.com>
2014-11-09 17:14:40 -0500

    WIP: clusterTimeSeries

--- R/clusterTimeSeries.R
+++ R/clusterTimeSeries.R
@@ -1,20 +1,27 @@
-clusterTimeSeries <- function(  img, mask, krange=2:10,
-  nsvddims=NA ) {
+clusterTimeSeries <- function(  mat, krange=2:10,
+  nsvddims=NA, criterion='asw') {
   if (nargs() == 0) {
     print( args( clusterTimeSeries ) )
     return(1)
   }
+  pckg <- try(require(fpc))
+  if (!pckg) {
+    cat("Installing 'fpc' from CRAN\n")
+    getPckg("fpc")
+    require("fpc")
+  }
   if ( is.na(nsvddims) ) nsvddims<-(max(krange)*2)
-  mat<-timeseries2matrix( img, mask )
+#  mat<-timeseries2matrix( img, mask )
   if ( nsvddims > nrow(mat) ) nsvddims<-nrow(mat)/2
   matsvd<-svd( mat, nu=nsvddims, nv=0 )
-  pk<-pamk( mat, krange=krange )
+  pk<-pamk( mat, krange=krange, criterion=criterion )
   clusters<-pk$pamobject$clustering
   tsimagelist<-list()
   for ( i in 1:max(clusters) )
     {
-    kmat<-mat[ clusters==i  ,  ]
-    tsimagelist[[i]]<-matrix2timeseries( img, mask, kmat  )
+    kmat<-mat[ clusters!=i  ,  ]
+    tsimagelist[[i]]<-kmat
+    # matrix2timeseries( img, mask, kmat  )
     }
   return( list(kimgs=tsimagelist, clusters=clusters) )
 }
