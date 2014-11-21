75556d581f4f0fe3401e0624c5b38ab77fb8a91f
BBAvants <stnava@gmail.com>
2014-11-21 17:44:53 -0500

    ENH: feathering

--- R/exemplarInpainting.R
+++ R/exemplarInpainting.R
@@ -6,8 +6,8 @@
 #' @param painting mask with values 1 or values 1 and 2 - if there is a 2 then it will learn from label 1 to paint label 2
 #' @param a list containing antsImages
 #' @param featureRadius - radius of image neighborhood e.g. 2
-#' @param putbackoriginaldata - put values in label 1 in approximated image
 #' @param sharpen - sharpen the approximated image
+#' @param feather - value (e.g. 1) that helps feather the mask for smooth blending
 #' @return inpainted image
 #' @author Brian B. Avants
 #' @keywords inpainting template
@@ -26,7 +26,7 @@
 #' painted<-exemplarInpainting(fi,mask,ilist)
 exemplarInpainting<-function( img, paintMask,
   imageList, featureRadius=2,
-  putbackoriginaldata=FALSE, sharpen=FALSE )
+  sharpen=FALSE, feather=1 )
 {
 mask<-antsImageClone( paintMask )
 mask[ paintMask != 1 ]<-0 # dont use the lesion
@@ -36,11 +36,21 @@ if ( inpaintLesion )
   {
   lmask<-antsImageClone( paintMask )
   lmask[ paintMask != 2 ]<-0 # use the lesion
+  lmask[ paintMask == 2 ]<-1 # use the lesion
+  fmask<-antsImageClone( paintMask )
+  fmask[ paintMask < 0.5 ]<-0  # full mask
+  fmask[ paintMask >= 0.5 ]<-1 #
+  featherMask<-antsImageClone(lmask)
+  SmoothImage(img@dimension,featherMask,feather,featherMask)
+  featherMask2<-antsImageClone(lmask)
+  featherMask2[ featherMask2 >= 0 ]<-1.0
+  featherMask2[ featherMask > 0 ]<-1.0-featherMask[ featherMask > 0 ]
   }
 targetvoxels<-img[ mask == 1 ]
 radius <- rep(featureRadius,img@dimension)
 nmat<-matrix()
 lmat<-matrix()
+fmat<-matrix()
 for ( i in ilist )
   {
   mat<-(antsGetNeighborhoodMatrix( i, mask, radius,
@@ -51,6 +61,9 @@ for ( i in ilist )
     mat<-(antsGetNeighborhoodMatrix(i,lmask,radius,
       boundary.condition='mean'))
     if ( all(dim(lmat)==1) ) lmat<-t(mat) else lmat<-cbind(lmat,t(mat))
+    mat<-(antsGetNeighborhoodMatrix(i,fmask,radius,
+      boundary.condition='mean'))
+    if ( all(dim(fmat)==1) ) fmat<-t(mat) else fmat<-cbind(fmat,t(mat))
     }
   }
 nmatdf<-data.frame(nmat)
@@ -60,23 +73,24 @@ if (  nrow(nmatdf) != length(targetvoxels) )
   return( mask )
   }
 mdl<-rlm( targetvoxels ~ ., data=nmatdf )
-if ( inpaintLesion )
+if ( inpaintLesion == FALSE )
   {
-  lmatdf<-data.frame(lmat)
-  lesvox<-predict(mdl,newdata=lmatdf)
+  pvox<-predict(mdl,type='response')
+  predimg<-makeImage( mask, pvox )
+  return(predimg)
   }
-pvox<-predict(mdl,type='response')
-if (  putbackoriginaldata )
- pvox<-img[ mask == 1 ]
-predimg<-makeImage( mask, pvox )
-predimg[ mask == 1 ]   <- pvox
-if ( inpaintLesion )
-  predimg[ paintMask == 2  ] <- lesvox
+# otherwise predict from full mat and feather-combine
+lmatdf<-data.frame(fmat)
+lesvox<-predict(mdl,newdata=lmatdf)
+predimg<-makeImage( fmask, lesvox )
 if ( sharpen )
   ImageMath(img@dimension,predimg,'Sharpen',predimg)
-if (  putbackoriginaldata )
- predimg[ mask == 1 ]<-img[ mask == 1 ]
-return(predimg)
+# now make two vectors - one for the lesion and
+# one for the original image
+vec1<-img[ fmask == 1 ]*featherMask2[fmask==1]
+vec2<-predimg[ fmask == 1 ]*featherMask[fmask==1]
+predimg[fmask==1]<-vec2+vec1
+return( predimg )
 # for bayesian regression - amazingly fast!
 # W2 = invcov.shrink(t(nmat), 0.1)
 }
