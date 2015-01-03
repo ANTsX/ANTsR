9b02e0782abc13183a04ad1166a539b719632625
stnava <stnava@gmail.com>
2015-01-02 19:52:12 -0600

    STYLE: need pixel type checking

--- R/antsImageMutualInformation.R
+++ R/antsImageMutualInformation.R
@@ -2,5 +2,12 @@ antsImageMutualInformation <- function(in_image1, in_image2) {
   if (length(dim(in_image1)) == 1)
     if (dim(in_image1)[1] == 1)
       return(NULL)
+  if ( in_image1@pixeltype != "float" |
+       in_image2@pixeltype != "float"   )
+       {
+       print(args(antsImageMutualInformation))
+       print("input images must have float pixeltype")
+       return(NA)
+       }
   .Call("antsImageMutualInformation", in_image1, in_image2, PACKAGE = "ANTsR")
 }
