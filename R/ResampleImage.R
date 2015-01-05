06862106ae14ab4b7707f17f1322db3ff452ba82
stnava <stnava@gmail.com>
2015-01-05 11:00:39 -0600

    ENH: ResampleImage.R

--- R/ResampleImage.R
+++ R/ResampleImage.R
@@ -1,3 +1,13 @@
 ResampleImage <- function(...) {
-  .Call("ResampleImage", as.character(c(...)))
-} 
+  veccer<-c(...)
+  veccer2<-veccer
+  img1<-antsImageClone(veccer[[2]])
+  img1d<-antsImageClone(img1,"double")
+  img2<-antsImageClone(veccer[[3]])
+  img2d<-antsImageClone(img2,"double")
+  veccer2[[2]]<-img1d
+  veccer2[[3]]<-img2d
+  .Call("ResampleImage", int_antsProcessArguments(veccer2) )
+  outimg<-antsImageClone( veccer2[[3]],img1@pixeltype)
+  return( outimg )
+}
