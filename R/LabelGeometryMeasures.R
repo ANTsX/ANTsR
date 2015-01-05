e593ec816d8b3d9ba2a12c05057d333240fbc3c9
stnava <stnava@gmail.com>
2015-01-05 12:40:46 -0600

    WIP: LabelGeometryMeasures in antsr

--- R/LabelGeometryMeasures.R
+++ R/LabelGeometryMeasures.R
@@ -1,3 +1,11 @@
 LabelGeometryMeasures <- function(...) {
-  .Call("LabelGeometryMeasures", as.character(c(...)))
-} 
+  veccer<-c(...)
+  veccer2<-veccer
+  if ( length(veccer) > 1 )
+  {
+  img1<-antsImageClone(veccer[[2]])
+  img1int<-antsImageClone(img1,"unsigned int")
+  veccer2[[2]]<-img1int
+  }
+  .Call("LabelGeometryMeasures", int_antsProcessArguments(veccer2) )
+}
