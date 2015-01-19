866f66db412eb8f96dc0aa71983a14158da8a646
stnava <stnava@gmail.com>
2015-01-18 23:27:03 -0500

    WIP: comp time

--- R/imageFileNames2ImageList.R
+++ R/imageFileNames2ImageList.R
@@ -1,11 +1,12 @@
-imageFileNames2ImageList <- function(x, dim) {
+imageFileNames2ImageList <- function(x, dim, verbose=FALSE) {
   if (nargs() == 0) {
     print("Usage:  ilist<-imageFileNames2ImageList( x , imageDimension ) ")
     return(1)
   }
-  ilist <- list()
-  for (i in x) {
-    ilist <- lappend(ilist, antsImageRead(i, dim))
+  ilist <- as.list(rep(NA,length(x)))
+  for (i in 1:length(x)) {
+    if ( i %% 100 == 1 & verbose ) { print(i); print(Sys.time( )) }
+    ilist[[i]]<-antsImageRead(x[i],dim)
   }
   return(ilist)
-} 
+}
