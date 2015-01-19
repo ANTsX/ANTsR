e37aa7ca7b036ec6075a0b670f86cfe1bd52e35d
stnava <stnava@gmail.com>
2015-01-19 16:29:02 -0500

    STYLE: do some error checking

--- R/imagesToMatrix.R
+++ R/imagesToMatrix.R
@@ -1,4 +1,14 @@
 imagesToMatrix <- function(imageList, mask) {
   n<-length( imageList )
+  if ( n < 1 )
+    {
+    print(" length of input list must be >= 1 ")
+    return(NA)
+    }
+  if ( class(imageList) != "character" )
+    {
+    print("Must pass a list of filenames")
+    return(NA)
+    }
   return( .Call("imagesToMatrix", imageList, mask, n ) )
 }
