9410a6d5e46098875446cb3ecb392de1b4bd1f00
Ben Kandel <bkandel@seas.upenn.edu>
2015-01-20 17:51:36 -0500

    New function crossvalidatedR2.
    crossvalidatedR2 calculates a k-fold cross-validation prediction accuracy from a predictor matrix with optional covariates when predicting an outcome matrix.

---
+++ R/crossvalidatedR2.R
@@ -0,0 +1,55 @@
+#' @name crossvalidatedR2
+#' @title Cross-Validated R^2 value
+#' @description Computes an R^2 value for predicting an outcome measure using a k-fold cross-validation scheme.
+#' @usage crossvalidatedR2(x, y, ngroups=5, covariates=NA)
+#' @param x Input predictor matrix.
+#' @param y Target dependent variable.
+#' @param ngroups Number of cross-validation folds to use.
+#' @param covariates Covariate predictors.
+#' @return Matrix of size \code{ngroups} by \code{ncol(x)}, which each row corresponding to one fold and the columns corresponding to the R2 values for each predictor.
+#' @author Brian B Avants, Benjamin M. Kandel
+#' @examples
+#' set.seed(300)
+#' ncol <- 30
+#' nrow <- 20 
+#' covariate <- sin((1:nrow)*2*pi/nrow)
+#' x <- matrix(rep(NA, nrow*ncol), nrow=nrow)
+#' xsig <- seq(0,1,length.out=nrow)
+#' y <- xsig + covariate + rnorm(nrow, sd=0.5) 
+#' for(i in 1:ncol){
+#'   x[, i] <- xsig + rnorm(nrow, sd=i/ncol)
+#' }
+#' r2 <- crossvalidatedR2(x, y, covariates=covariate) 
+
+crossvalidatedR2 <- function(x, y, ngroups=5, covariates=NA) {
+  nvox <- ncol(x)
+  R2 <- matrix(rep(0, ngroups*nvox), nrow = ngroups)
+  groups <- c()
+  grouplength <- round(nrow(x)/ngroups) - 1
+  for (k in 1:ngroups) 
+    groups <- c(groups, rep(k, grouplength))
+  groups <- c(rep(1, nrow(x) - length(groups)), groups)
+  if(!is.matrix(covariates)) 
+    covariates <- as.matrix(covariates)
+
+  for (k in 1:ngroups) {
+    selector <- groups != k
+    mydf <- data.frame(y[selector])
+    if (!all(is.na(covariates)))
+      mydf <- data.frame(mydf, covariates[selector, ])
+    mylm1 <- lm(x[selector, ] ~ ., data = mydf)
+    selector <- groups == k
+    mydf <- data.frame(y[selector])
+    if (!all(is.na(covariates)))
+      mydf <- data.frame(mydf, covariates[selector, ])
+    predmat <- predict(mylm1, newdata = mydf)
+    realmat <- x[selector, ]
+    for (v in 1:nvox)
+      {
+      sum1<-sum((predmat[, v] - realmat[,v])^2,na.rm=T)
+      sum2<-sum((mean(realmat[, v],na.rm=T)- realmat[, v])^2, na.rm=T)
+      R2[k, v] <- 100 * (1 - sum1/sum2 )
+      }
+  }
+  return(R2)
+}
