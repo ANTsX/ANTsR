2598cb4c1b3a95b3a5dc2b754a8e8ba9f60f3d84
stnava <stnava@gmail.com>
2015-01-22 20:11:25 -0500

    ENH: kmeans

---
+++ R/kmeansSegmentation.R
@@ -0,0 +1,21 @@
+#' @name kmeansSegmentation
+#' @title k means image segmentation.
+#' @usage kmeansSegmentation(img,3)
+#' @param k integer number of classes
+#' @return segmentation and probability images
+#' @author Brian B. Avants
+#' @examples
+#' fi<-antsImageRead( getANTsRData('r16') ,2)
+#' seg<-kmeansSegmentation( fi, 3 )
+kmeansSegmentation <- function(img,k,kmask=NA,mrf=0.1){
+  kmimg<-antsImageClone(img)
+  dim<-img@dimension
+  ImageMath(dim,kmimg,"Normalize", kmimg )
+  if ( is.na(kmask) ) kmask<-getMask(kmimg,0.01,1,cleanup = 2 )
+  ImageMath(dim,kmask,"FillHoles",kmask)
+  nhood<-paste(rep(1,dim),collapse='x')
+  mrf<-paste("[",mrf,",",nhood,"]")
+  kmimg<-Atropos( d = dim, a = kmimg, m = mrf,c = "[5,0]",
+      i = paste("kmeans[",k,"]",sep=''), x = kmask)
+  return(kmimg)
+}
