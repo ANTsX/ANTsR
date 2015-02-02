59712436dc53bf55e76fae15afbdc35dbcdc548d
stnava <stnava@gmail.com>
2015-02-01 21:21:28 -0500

    COMP: simplify by rm doc

--- doc/scripts/convertRd2rst.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript 
-# get the script name (only works when invoked with Rscript).
-library(ANTsR)
-Args <- commandArgs()
-self <- Args[4]
-self <- substring(self, 8, nchar(as.character(self)))
-fn <- (Args[6])
-ofn <- (Args[7])
-convertRd2rst(fn, ofn) 
--- doc/scripts/convertRd2rst.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for x in ../man/*Rd ; do 
-  y=${x%.Rd}
-  y=` basename $y `
-  onm=./source/Rd_${y}.Rrst 
-  if [[ ! -s ${onm} ]] ; then 
-    ./scripts/convertRd2rst.R $x $onm
-  fi 
-done
