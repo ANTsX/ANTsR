6819006f0ed1e786208e302ea43eeb76f9695845
stnava <stnava@gmail.com>
2015-02-02 16:14:06 -0500

    WIP: ... onload becomes onattach

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,5 +1,5 @@
 
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
   # .libPaths( c( .libPaths(),
   # '/Users/stnava/code/gitANTs/ANTsR/src/ANTS/ANTS-build/lib/' ) )
   packageStartupMessage("Welcome to ANTsR")
