0752c9f2f70afca58d73d80e15b3bc022a13f9d5
stnava <stnava@gmail.com>
2015-02-04 17:41:16 -0500

    DOC: SmoothImage

--- R/KellyKapowski.R
+++ R/KellyKapowski.R
@@ -1,29 +1,31 @@
 #' Compute cortical thickness using the DiReCT algorithm.
-#' 
+#'
 #' Diffeomorphic registration-based cortical thickness based on probabilistic
 #' segmentation of an image.  This is an optimization algorithm.
-#' 
-#' 
-#' @param imageDimension2|3|4 Number of dimensions of the input image
-#' @param inputImage Input image to operate on
+#'
+#'
+#' @param d Number of dimensions of the input image
 #' @param s segmentation image
 #' @param g gray matter probability image
 #' @param w white matter probability image
 #' @param c convergence params - first controls iterations
 #' @param r gradient descent update parameter
 #' @param m gradient field smoothing parameter
+#' @param outimg output image
+#' @param ... anything else, see KK help in ANTs
 #' @return 0 -- Success\cr 1 -- Failure
 #' @author Shrinidhi KL, Avants BB
 #' @examples
-#' 
+#'
 #' \dontrun{
 #' KellyKapowski( d=3, s=simg, g=gimg,w=wimg,c=45,r=0.5,m=1,o=oimg )
 #' }
-#' 
+#'
 #' @export KellyKapowski
-KellyKapowski <- function(d = NA, s = NA, g = NA, w = NA, c = "[45,0.0,10]", r = 0.025, 
-  m = 1.5, outimg = outimg, ...) {
-  if (missing(d) | missing(s) | missing(g) | missing(w) | missing(c) | missing(r) | 
+KellyKapowski <- function(d = NA, s = NA, g = NA, w = NA,
+   c = "[45,0.0,10]", r = 0.025,
+   m = 1.5, outimg = outimg, ...) {
+  if (missing(d) | missing(s) | missing(g) | missing(w) | missing(c) | missing(r) |
     missing(m) | missing(outimg)) {
     print("Input error - check params & usage")
     return(NULL)
@@ -34,4 +36,4 @@ KellyKapowski <- function(d = NA, s = NA, g = NA, w = NA, c = "[45,0.0,10]", r =
   # KellyKapowski( d=3, s=simg, g=gimg,w=wimg,c=10,r=0.5,m=1,o=oimg )
   kkargs <- list(d = d, s = s, g = g, w = w, c = 10, r = 0.5, m = 1, outimg = outimg)
   .Call("KellyKapowski", .int_antsProcessArguments(c(kkargs)), PACKAGE = "ANTsR")
-} 
+}
--- R/MeasureMinMaxMean.R
+++ R/MeasureMinMaxMean.R
@@ -1,21 +1,22 @@
 #' Measure Min Max Mean
-#' 
+#'
 #' Measure min, max, and mean of image.
-#' 
-#' 
+#'
+#'
 #' @param image Input image of type \code{antsImage}.
+#' @param mask image mask
 #' @return List with entries \code{img.min}, \code{img.max}, \code{img.mean}.
 #' @author Kandel BM.
 #' @examples
-#' 
+#'
 #' \dontrun{
 #'   myimg <- as.antsImage(array(rnorm(1000), dim=c(10, 10, 10)))
 #'   MeasureMinMaxMean(myimg)
 #' }
-#' 
+#'
 #' @export MeasureMinMaxMean
 MeasureMinMaxMean <- function(image, mask = NA) {
-  if (missing(image)) 
+  if (missing(image))
     stop("Missing input image.")
   if (!is.na(mask)) {
     imagevec <- image[mask > 0.5]
@@ -28,4 +29,4 @@ MeasureMinMaxMean <- function(image, mask = NA) {
   img.mean <- mean(as.array(image))
   img.max <- max(as.array(image))
   list(img.min = img.min, img.max = img.max, img.mean = img.mean)
-} 
+}
