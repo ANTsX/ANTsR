90e38dbf85b00aba0535b7022bad43089a1472b5
stnava <stnava@gmail.com>
2015-02-04 17:47:30 -0500

    BUG: in abpn4 doc

--- R/abpN4.R
+++ R/abpN4.R
@@ -1,27 +1,25 @@
 #' N4 with previously set parameters based on ants brain processing strategies.
-#' 
+#'
 #' Truncate outlier intensities and bias correct with N4 with or without mask
 #' and weight images.
-#' 
-#' 
-#' @param image image to be bias corrected
+#'
+#'
+#' @param img image to be bias corrected
 #' @param intensityTruncation Params to TruncateImageIntensity in ImageMath
 #' @param mask optional antsImage mask
-#' @param weightimage optional antsImage weighting
+#' @param weightimg optional antsImage weighting
 #' @param usen3 Use N3 instead of N4
 #' @return outputs a bias corrected image. 1 -- Failure
 #' @author Tustison N, Avants BB
 #' @examples
-#' 
-#' \dontrun{
-#' fn<-getANTsRData('r16') 
+#'
+#' fn<-getANTsRData('r16')
 #' img<-antsImageRead(fn,2)
 #' img2<-abpN4( img )
-#' }
-#' 
+#'
 #' @export abpN4
-abpN4 <- function(img = NA, intensityTruncation = c(0.025, 0.975, 256), mask = NA, 
-  weightimg = NA, usen3 = FALSE) {
+abpN4 <- function(img = NA, intensityTruncation = c(0.025, 0.975, 256),
+  mask = NA, weightimg = NA, usen3 = FALSE) {
   numargs <- nargs()
   if (numargs < 1 | missing(img) | class(img)[1] != "antsImage") {
     cat(" abpN4( img = inimg , intensityTruncation=c( 0.025, 0.975, 256 ), mask=NA, weightimg=NA, usen3=FALSE ) \n")
@@ -33,7 +31,7 @@ abpN4 <- function(img = NA, intensityTruncation = c(0.025, 0.975, 256), mask = N
   }
   outimg <- antsImageClone(img, "float")
   dim <- as.character(img@dimension)
-  ImageMath(dim, outimg, "TruncateImageIntensity", outimg, intensityTruncation[1], 
+  ImageMath(dim, outimg, "TruncateImageIntensity", outimg, intensityTruncation[1],
     intensityTruncation[2], intensityTruncation[3])
   if (usen3 == TRUE) {
     N3BiasFieldCorrection(list(img@dimension, outimg, outimg, "4"))
@@ -46,23 +44,23 @@ abpN4 <- function(img = NA, intensityTruncation = c(0.025, 0.975, 256), mask = N
   N4_SHRINK_FACTOR_2 <- "2"
   N4_BSPLINE_PARAMS <- "[200]"
   if (is.na(mask)) {
-    N4BiasFieldCorrection(list(d = outimg@dimension, i = outimg, s = N4_SHRINK_FACTOR_1, 
+    N4BiasFieldCorrection(list(d = outimg@dimension, i = outimg, s = N4_SHRINK_FACTOR_1,
       c = N4_CONVERGENCE_1, b = N4_BSPLINE_PARAMS, o = outimg))
     return(outimg)
   }
   if (is.na(weightimg)) {
-    N4BiasFieldCorrection(list(d = outimg@dimension, i = outimg, s = N4_SHRINK_FACTOR_1, 
+    N4BiasFieldCorrection(list(d = outimg@dimension, i = outimg, s = N4_SHRINK_FACTOR_1,
       c = N4_CONVERGENCE_1, b = N4_BSPLINE_PARAMS, x = mask, o = outimg))
-    N4BiasFieldCorrection(list(d = outimg@dimension, i = outimg, s = N4_SHRINK_FACTOR_2, 
+    N4BiasFieldCorrection(list(d = outimg@dimension, i = outimg, s = N4_SHRINK_FACTOR_2,
       c = N4_CONVERGENCE_2, b = N4_BSPLINE_PARAMS, x = mask, o = outimg))
   }
   if (!is.na(weightimg)) {
-    N4BiasFieldCorrection(list(d = outimg@dimension, i = outimg, s = N4_SHRINK_FACTOR_1, 
-      c = N4_CONVERGENCE_1, b = N4_BSPLINE_PARAMS, x = mask, w = weightimg, 
+    N4BiasFieldCorrection(list(d = outimg@dimension, i = outimg, s = N4_SHRINK_FACTOR_1,
+      c = N4_CONVERGENCE_1, b = N4_BSPLINE_PARAMS, x = mask, w = weightimg,
       o = outimg))
-    N4BiasFieldCorrection(list(d = outimg@dimension, i = outimg, s = N4_SHRINK_FACTOR_2, 
-      c = N4_CONVERGENCE_2, b = N4_BSPLINE_PARAMS, x = mask, w = weightimg, 
+    N4BiasFieldCorrection(list(d = outimg@dimension, i = outimg, s = N4_SHRINK_FACTOR_2,
+      c = N4_CONVERGENCE_2, b = N4_BSPLINE_PARAMS, x = mask, w = weightimg,
       o = outimg))
   }
   return(outimg)
-} 
+}
