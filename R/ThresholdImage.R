c9203ba181a0daec60c0fe82f3860a465f021f1d
stnava <stnava@gmail.com>
2015-02-05 18:59:10 -0500

    BUG: ThresholdImage reversal of in/out values

--- R/ThresholdImage.R
+++ R/ThresholdImage.R
@@ -1,20 +1,21 @@
-#' @name ThresholdImage 
-#' @title Threshold Image 
+#' @name ThresholdImage
+#' @title Threshold Image
 #' @usage ThresholdImage(dim, inimg, outimg, lothresh, hithresh, inval=0, outval=1)
 #' @param dim Dimension of the input image
 #' @param inimg Input image to operate on
 #' @param outimg Result image
 #' @param lothresh Lower edge of threshold window
 #' @param hithresh Higher edge of threshold window
-#' @param inval Output value for image voxels in between \code{lothresh} and \code{hithresh} 
-#' @param outval Output value for image voxels lower than \code{lothresh} or higher than \code{hithresh} 
+#' @param inval Output value for image voxels in between \code{lothresh} and \code{hithresh}
+#' @param outval Output value for image voxels lower than \code{lothresh} or higher than \code{hithresh}
 #' @return 0 -- Success\cr 1 -- Failure
 #' @author Shrinidhi KL
 #' @examples
-#' img <- makeImage(c(5,5), rnorm(25)) 
+#' img <- makeImage(c(5,5), rnorm(25))
 #' ThresholdImage(2, img, img, 0.5, 1e3)
 #' @export ThresholdImage
-ThresholdImage <- function(dim, inimg, outimg, lothresh, hithresh, inval=0, outval=1) {
+ThresholdImage <- function(dim, inimg, outimg,
+  lothresh, hithresh, inval=1, outval=0) {
   args <- list(dim, inimg, outimg, lothresh, hithresh, inval, outval)
   .Call("ThresholdImage", .int_antsProcessArguments(args), PACKAGE = "ANTsR")
-} 
+}
