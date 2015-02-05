095a1c905563a1881e7797b0d22e0bc0bbf08b50
Ben Kandel <bkandel@seas.upenn.edu>
2015-02-05 10:37:36 -0500

    COMP: Explicit arguments in ThresholdImage.
    CRAN check complains about having documentation that lists specific arguments, but passing all arguments to a .Call using '...'.  This commit lists arguments for ThresholdImage explicitly to avoid that problem.

--- R/ThresholdImage.R
+++ R/ThresholdImage.R
@@ -1,8 +1,8 @@
 #' @name ThresholdImage 
 #' @title Threshold Image 
-#' @usage ThresholdImage(dim, inim, outimg, lothresh, hithresh, inval=0, outval=1)
+#' @usage ThresholdImage(dim, inimg, outimg, lothresh, hithresh, inval=0, outval=1)
 #' @param dim Dimension of the input image
-#' @param inim Input image to operate on
+#' @param inimg Input image to operate on
 #' @param outimg Result image
 #' @param lothresh Lower edge of threshold window
 #' @param hithresh Higher edge of threshold window
@@ -14,6 +14,7 @@
 #' img <- makeImage(c(5,5), rnorm(25)) 
 #' ThresholdImage(2, img, img, 0.5, 1e3)
 #' @export ThresholdImage
-ThresholdImage <- function(...) {
-  .Call("ThresholdImage", .int_antsProcessArguments(c(...)), PACKAGE = "ANTsR")
+ThresholdImage <- function(dim, inimg, outimg, lothresh, hithresh, inval=0, outval=1) {
+  args <- list(dim, inimg, outimg, lothresh, hithresh, inval, outval)
+  .Call("ThresholdImage", .int_antsProcessArguments(args), PACKAGE = "ANTsR")
 } 
