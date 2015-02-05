482f150816357706b1162bd86f17492a3bfa27eb
Ben Kandel <bkandel@seas.upenn.edu>
2015-02-05 10:19:32 -0500

    DOC: ThresholdImage

--- R/ThresholdImage.R
+++ R/ThresholdImage.R
@@ -1,25 +1,18 @@
-#' Theshold Image
-#' 
-#' Perform thresholding on the given image
-#' 
-#' 
-#' @param imageDimension2|3 Number of dimensions of the input image
-#' @param inputImage Input image to operate on
-#' @param outputImage Result image
-#' @param thresh-low Lower edge of threshold window
-#' @param thresh-high Higher edge of threshold window
-#' @param inside-value Inside value
-#' @param outside-value Outside value
-#' @param number-of-thresholds Number of thresholds
+#' @name ThresholdImage 
+#' @title Threshold Image 
+#' @usage ThresholdImage(dim, inim, outimg, lothresh, hithresh, inval=0, outval=1)
+#' @param dim Dimension of the input image
+#' @param inim Input image to operate on
+#' @param outimg Result image
+#' @param lothresh Lower edge of threshold window
+#' @param hithresh Higher edge of threshold window
+#' @param inval Output value for image voxels in between \code{lothresh} and \code{hithresh} 
+#' @param outval Output value for image voxels lower than \code{lothresh} or higher than \code{hithresh} 
 #' @return 0 -- Success\cr 1 -- Failure
 #' @author Shrinidhi KL
 #' @examples
-#' 
-#' \dontrun{
-#' ThresholdImage( 3 , 'input_img.nii' , 'output_img.nii' , 2 , 3 )
-#' ThresholdImage( 3 , 'input_img.nii' , 'output_img.nii' , 'Otsu' , 3 )
-#' }
-#' 
+#' img <- makeImage(c(5,5), rnorm(25)) 
+#' ThresholdImage(2, img, img, 0.5, 1e3)
 #' @export ThresholdImage
 ThresholdImage <- function(...) {
   .Call("ThresholdImage", .int_antsProcessArguments(c(...)), PACKAGE = "ANTsR")
