c34a62e6c72b6f23b17812c97ad6f9de959294b7
armaneshaghi <arman.eshaghi@gmail.com>
2015-02-10 09:22:39 +0000

    another retry to get coverage for first time

--- tests/testthat/test-antsImageRead.R
+++ tests/testthat/test-antsImageRead.R
@@ -1,6 +1,6 @@
 context("Reading image")
 
-testthat("image can be read", {
+test_that("image can be read", {
          img<-antsImageRead( getANTsRData("r16"), 2 )
          pixeltype = "float"
          expect_match(img@pixeltype, pixeltype)
