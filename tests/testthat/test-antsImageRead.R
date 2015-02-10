5241310bd93f0f6c30ab5833c151de780b070f6e
armaneshaghi <arman.eshaghi@gmail.com>
2015-02-10 13:01:58 +0000

    temporarily remove empty test files for coverage numbers

--- tests/testthat/test-antsImageRead.R
+++
@@ -1,7 +0,0 @@
-context("Reading image")
-
-test_that("image can be read", {
-         img<-antsImageRead( getANTsRData("r16"), 2 )
-         pixeltype = "float"
-         expect_match(img@pixeltype, pixeltype)
-})
