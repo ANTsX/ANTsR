a290dd69311977a5ffdf22de0c486a9c1afe72ea
armaneshaghi <arman.eshaghi@gmail.com>
2015-02-11 15:09:58 +0000

    COMP: travis_tool run_test did not work

--- tests/testthat/test-Atropos.R
+++ tests/testthat/test-Atropos.R
@@ -7,5 +7,5 @@ test_that("image can be segmented with Atropos", {
           mask[img<=10] <- 0
           segs1<-Atropos( d = img@dimension, a = img, m = "[0.2,1x1]",c = "[5,0]",  i = "kmeans[3]", x =mask)
           csf <- antsImageRead("data/csf.nii.gz")
-          expect_true(mean(csf) == mean(probabilityimages[[1]])
+          expect_true(mean(csf) == mean(segs1$probabilityimages[[1]])
 })
