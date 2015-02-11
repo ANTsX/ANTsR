38b17bf47e5293b94f621118262f6de7c0aa50d3
Ben Kandel <bkandel@seas.upenn.edu>
2015-02-11 11:36:48 -0500

    BUG: Atropos test didn't work.
    In this version of Atropos-test, we explicitly test for similarity in voxel classification.  Up to 0.1% of masked voxels may be classified differently to allow for some randomization in initialization.

--- tests/testthat/test-Atropos.R
+++ tests/testthat/test-Atropos.R
@@ -5,7 +5,12 @@ test_that("image can be segmented with Atropos", {
           mask <- antsImageClone(img)
           mask[img>10] <- 1
           mask[img<=10] <- 0
+          nvox <- sum(as.array(mask))
           segs1<-Atropos( d = img@dimension, a = img, m = "[0.2,1x1]",c = "[5,0]",  i = "kmeans[3]", x =mask)
-          csf <- antsImageRead("data/csf.nii.gz")
-          expect_true(mean(csf) == mean(segs1$probabilityimages[[1]])
+          seg <- antsImageRead('data/atroposseg.nii.gz', 2)
+          # condition for passing is that less than 0.1% of voxels are classified
+          # differently from our standard to allow for randomness in initialization.
+          expect_true(
+           sum(abs(as.array(seg) - as.array(segs1$segmentation))) < nvox*1e-3) 
+ 
 })
