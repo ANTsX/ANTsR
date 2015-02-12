c2a4f094a2b6120f4f936b382efa14717c3380fb
Ben Kandel <bkandel@seas.upenn.edu>
2015-02-12 10:12:08 -0500

    ENH: Less verbose startup.
    In keeping with the wishes of R-core (https://stat.ethz.ch/pipermail/r-devel/2011-June/061277.html), this commit makes using library(ANTsR) silent.

--- R/onAttach.R
+++
@@ -1,6 +0,0 @@
-
-.onAttach <- function(libname, pkgname) {
-  # .libPaths( c( .libPaths(),
-  # '/Users/stnava/code/gitANTs/ANTsR/src/ANTS/ANTS-build/lib/' ) )
-  packageStartupMessage("Welcome to ANTsR")
-} 
