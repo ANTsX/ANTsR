e881faa7d45cf43decc792e94f955dea2384e99c
Ben Kandel <bkandel@seas.upenn.edu>
2015-02-11 21:30:02 -0500

    ENH: More meaningful test for antsImageRead.

--- tests/testthat/test-antsImageRead.R
+++ tests/testthat/test-antsImageRead.R
@@ -2,6 +2,5 @@ context("Reading image")
 
 test_that("image can be read", {
          img<-antsImageRead( getANTsRData("r16"), 2 )
-         pixeltype = "float"
-         expect_match(img@pixeltype, pixeltype)
+         expect_true(sum(abs(as.array(img))) == 3307036) 
 })
