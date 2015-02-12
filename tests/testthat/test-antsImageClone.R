24c525c8f70dfa83f6b679aebbf934f710cd8c54
armaneshaghi <arman.eshaghi@gmail.com>
2015-02-12 16:47:41 +0000

    WIP: antsImageClone test
    A simple test for antsImageClone

---
+++ tests/testthat/test-antsImageClone.R
@@ -0,0 +1,8 @@
+
+context("Cloning image")
+
+test_that("image can be cloned", {
+         img1 <- antsImageRead( getANTsRData("r16"), 2 )
+         img2 <- antsImageClone(img1)
+         expect_true(sum(abs(as.array(img1))) == sum(abs(as.array(img2)))) 
+})
