d33b3c3c98bbeede086efd20fb4b850fcb916eb6
Jeffrey Duda <jeff.duda@gmail.com>
2015-02-13 10:53:16 -0500

    ADD: check for consistent physical space

---
+++ R/antsImagePhysicalSpaceConsistency.R
@@ -0,0 +1,55 @@
+#' Check for physical space consistency
+#'
+#' Check if two 'antsImage' objects occupy the same physical space
+#'
+#'
+#' @param img1 Image object of S4 class 'antsImage'.
+#' @param img2 Image object of S4 class 'antsImage'.
+#' @return Boolean indicating consistency of physical space
+#' @examples
+#'
+#' img1 <- makeImage(c(10,10),rnorm(100))
+#' img2 <- makeImage(c(10,10),rnorm(100))
+#' check <- antsImagePhysicalSpaceConsistency(img1,img2)
+#'
+#'
+#' @export antsCopyImageInfo
+antsImagePhysicalSpaceConsistency <- function(img1, img2, coordinate.tolerance=1e-6, direction.tolerance=1e-6) {
+  if (!(is.antsImage(img1)) || !(is.antsImage(img2))) {
+    stop("Both inputs must be of class 'antsImage'")
+  }
+
+  # Image dimension check
+  if ( length( which(dim(img1) != dim(img2)) ) > 0 )
+  {
+    #print( "Images are of different size")
+    return(FALSE)
+  }
+
+  # Image spacing check
+  cTol = coordinate.tolerance * antsGetSpacing(img1)[1]
+  spDiff = abs(antsGetSpacing(img1) - antsGetSpacing(img2))
+  if (length( which(spDiff > cTol)) > 0)
+  {
+    #print( "Images have inconsistent spacing")
+    return(FALSE)
+  }
+
+  # Image origin check
+  ogDiff = abs(antsGetOrigin(img1) - antsGetOrigin(img2))
+  if (length( which(ogDiff > cTol)) > 0)
+  {
+    #print( "Images have inconsistent origin")
+    return(FALSE)
+  }
+
+  # Image direction check
+  dirDiff = abs(antsGetDirection(img1) - antsGetDirection(img2))
+  if ( length( which(dirDiff > direction.tolerance) ) > 0)
+  {
+    #print( "Image have different directions")
+    return(FALSE)
+  }
+
+  return(TRUE)
+}
