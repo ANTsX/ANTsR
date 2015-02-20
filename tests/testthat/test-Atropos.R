494f903e25c28bf9fb1a1bc27afc63feda601e01
stnava <stnava@gmail.com>
2015-02-20 06:16:34 -0800

    STYLE: caml conversion done

--- tests/testthat/test-Atropos.R
+++ tests/testthat/test-Atropos.R
@@ -1,12 +1,12 @@
 context("segmenting image")
 
-test_that("image can be segmented with Atropos", {
+test_that("image can be segmented with atropos", {
           img <- antsImageRead( getANTsRData( "r16" ) , 2)
           mask <- antsImageClone(img)
           mask[img>10] <- 1
           mask[img<=10] <- 0
           nvox <- sum(as.array(mask))
-          segs1<-Atropos( d = img@dimension, a = img, m = "[0.2,1x1]",c = "[5,0]",  i = "kmeans[3]", x =mask)
+          segs1<-atropos( d = img@dimension, a = img, m = "[0.2,1x1]",c = "[5,0]",  i = "kmeans[3]", x =mask)
           seg <- antsImageRead(getANTsRData( "atroposseg" ), 2)
           # condition for passing is that less than 0.1% of voxels are classified
           # differently from our standard to allow for randomness in initialization.
@@ -14,4 +14,4 @@ test_that("image can be segmented with Atropos", {
            sum(abs(as.array(seg) - as.array(segs1$segmentation))) < nvox*1e-1)
 
 })
-#todo : other segmentation approaches of Atropos
+#todo : other segmentation approaches of atropos
