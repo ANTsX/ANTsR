a6a61ecf22a3c553d768435ff344c246008eb7fe
Ben Kandel <bkandel@seas.upenn.edu>
2015-03-12 12:19:34 -0400

    DOC: antsCopyImageInfo.

--- R/antsCopyImageInfo.R
+++ R/antsCopyImageInfo.R
@@ -1,24 +1,22 @@
 #' Copy header info
 #'
-#' Copy header info from one 'antsImage' to another
+#' Copy origin, direction, and spacing from one \code{antsImage} to another
 #'
 #'
-#' @param reference Image object of S4 class 'antsImage' to get values from.
-#' @param target Image object of S4 class 'antsImage' to copy values to.
-#' @return Matrix of image indices
+#' @param reference image object of S4 class 'antsImage' to get values from.
+#' @param target image object of S4 class 'antsImage' to copy values to.
+#' @return Target image with reference header information. 
 #' @examples
 #'
-#' img<-makeImage(c(10,10),rnorm(100))
-#' img2<-makeImage(c(10,10), rnorm(100))
-#' img2<-antsCopyImageInfo(img, img2)
-#'
+#' img <- makeImage(c(10,10),rnorm(100))
+#' img2 <- makeImage(c(10,10), rnorm(100))
+#' img2 <- antsCopyImageInfo(img, img2)
 #'
 #' @export antsCopyImageInfo
 antsCopyImageInfo <- function(reference, target) {
-  if (!(class(target) == "antsImage") || !(class(target) == "antsImage")) {
+  if (!(class(reference) == "antsImage") || !(class(target) == "antsImage")) {
     stop("Both inputs must be of class 'antsImage'")
   }
-
   antsSetOrigin(target, as.numeric(antsGetOrigin(reference)))
   antsSetDirection(target, antsGetDirection(reference))
   antsSetSpacing(target, as.numeric(antsGetSpacing(reference)))
