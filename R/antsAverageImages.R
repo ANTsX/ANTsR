238a0ad18dcff1006e17e941bd49fd661050acd9
stnava <stnava@gmail.com>
2015-03-12 11:40:06 -0400

    DOC: antsAverageImages

--- R/antsAverageImages.R
+++ R/antsAverageImages.R
@@ -1,32 +1,20 @@
-#' antsAverageImages
+#' averages an image list with or without intensity normalization
 #'
 #' calculate the mean of a list of antsImages imageList: can contain anything that be cast via as.array()
 #' @param imageList must be an antsImage list
 #' @param reference must be an antsImage of the same size as those in the list
-#' @param normalize true or false
-#' @author Avants BB
-antsAverageImages <- function(imageList, reference, normalize = FALSE) {
-  
-  # FIXME - input checking here
-  if (class(reference) != "antsImage") {
-    stop("reference must be of class 'antsImage'")
-  }
-  
-  template <- as.array(reference) * 0
-  
-  for (i in imageList) {
-    img <- as.array(i)
-    
-    if (length(which(dim(img) != dim(reference)) == FALSE) > 0) {
-      stop("Inputs and reference must all have same dimensions")
+#' @param normalize boolean determines if image is divided by mean before
+#' averaging
+#' @author Avants BB, Pustina D
+antsAverageImages <- function( imageList, normalize = FALSE )
+  {
+  avg <- imageList[[1]] * 0
+  for ( i in imageList ) {
+    if ( normalize ) {
+      i <- i / mean( img )
     }
-    
-    if (normalize) {
-      img <- img/mean(img)
-    }
-    template <- template + img
+    template <- template + i
   }
-  template <- as.antsImage(template/length(imageList))
-  antsCopyImageInfo(reference, template)
-  return(template)
-} 
+  avg <- avg / length(imageList)
+  return( avg )
+  }
