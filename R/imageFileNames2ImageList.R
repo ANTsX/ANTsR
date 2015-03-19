2936276798230372c1524a09459786af9fbf74b0
stnava <stnava@gmail.com>
2015-03-19 16:19:42 -0400

    DOC: no need for dim; also new antsimage stuff

--- R/imageFileNames2ImageList.R
+++ R/imageFileNames2ImageList.R
@@ -5,7 +5,7 @@
 #'
 #'
 #' @param x input file name list
-#' @param dim img dimensionality
+#' @param dim img dimensionality (optional)
 #' @return a list like this : mylist<-list( img1, img2 , etcetera ) is the
 #' output ... img* are antsImages
 #' @author Avants BB
@@ -14,15 +14,16 @@
 #' \dontrun{
 #'   # gglb<-paste('gmView1vec*.nii.gz',sep='')
 #'   # gfnl<-imageFileNames2ImageList( list.files(path=statdir,
-#'   #   pattern = glob2rx(gglb),full.names = T,recursive = T) , 3 )
+#'   #   pattern = glob2rx(gglb),full.names = T,recursive = T) )
 #' }
 #'
 #' @export imageFileNames2ImageList
-imageFileNames2ImageList <- function(x, dim) {
+imageFileNames2ImageList <- function( x, dim ) {
   if (nargs() == 0) {
     print("Usage:  ilist<-imageFileNames2ImageList( x , imageDimension ) ")
     return(1)
   }
+  if ( missing(dim) ) dim<-antsImageRead( x[1] )@dimension
   localreadfun <- function(x) {
     antsImageRead(x, dim)
   }
