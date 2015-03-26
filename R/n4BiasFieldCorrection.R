ac8b6630b35461b3c1439574c032d46dad05c0b7
Ben Kandel <bkandel@seas.upenn.edu>
2015-03-26 11:28:40 -0400

    ENH: enable parameter settings for n4

--- R/n4BiasFieldCorrection.R
+++ R/n4BiasFieldCorrection.R
@@ -4,7 +4,14 @@
 #'
 #' @param img input antsImage
 #' @param mask input mask, if one is not passed one will be made
-#' @return outimg
+#' @param shrinkFactor Shrink factor for multi-resolution correction, 
+#' typically integer less than 4
+#' @param convergence List of:  \code{iters}, vector of maximum number of 
+#' iterations for each shrinkage factor, and \code{tol}, the convergence tolerance.
+#' @param splineParam Parameter controlling number of control points in spline.
+#' Either single value, indicating how many control points, or vector 
+#' with one entry per dimension of image, indicating the spacing in each direction.
+#' @return outimg Bias-corrected image
 #' @author BB Avants
 #' @examples
 #'
@@ -12,15 +19,23 @@
 #'  n4img<-n4BiasFieldCorrection(img)
 #'
 #' @export n4BiasFieldCorrection
-n4BiasFieldCorrection<-function( img , mask=NA )
+n4BiasFieldCorrection<-function( img , mask=NA, shrinkFactor=4, 
+  convergence=list(iters=c(50,50,50,50), tol=0.0000001), 
+  splineParam=200)
 {
 if (!is.antsImage(mask)) 
   mask <- getMask(img) 
-N4_CONVERGENCE_1 <- "[50x50x50x50,0.0000001]"
-N4_CONVERGENCE_2 <- "[20x20x20x20,0.0000001]"
-N4_SHRINK_FACTOR_1 <- "4"
-N4_SHRINK_FACTOR_2 <- "2"
-N4_BSPLINE_PARAMS <- "[200]"
+N4_CONVERGENCE_1 <- paste("[", paste(convergence$iters, collapse="x"), 
+  ",", sprintf("%.10f", convergence$tol), "]", sep="") 
+N4_SHRINK_FACTOR_1 <- paste(shrinkFactor) 
+if (length(splineParam) == 1){
+  N4_BSPLINE_PARAMS <- paste("[", splineParam, "]", sep="") 
+} else if (length(splineParam) == img@dimension){
+  N4_BSPLINE_PARAMS <- paste("[", paste(splineParam, collapse="x"), "]", sep="")
+}  else {
+  stop("Length of splineParam must either be 1 or dimensionality of image.")
+}
+ 
 outimg<-antsImageClone(img)
 .helpn4BiasFieldCorrection(
   list(d = outimg@dimension,
