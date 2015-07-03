2c555861f8aab56693552de6b6a5d27f72d1902d
Jeffrey Duda <jeff.duda@gmail.com>
2015-07-03 14:34:34 -0400

    FIX: data type consistency

--- tests/testthat/test-antsImageWrite.R
+++ tests/testthat/test-antsImageWrite.R
@@ -5,6 +5,6 @@ test_that("image can be written with antsImageWrite", {
           img1_array <- array(rnorm(5*5), dim=c(5,5))
           img1 <- as.antsImage(img1_array, pixeltype="double")
           antsImageWrite(img1, 'output.nii.gz' )
-          img2 <- antsImageRead('output.nii.gz', 2)
+          img2 <- antsImageRead('output.nii.gz', 2, pixeltype="double")
           expect_equal(sum(abs(as.array(img2))), sum(abs(img1_array)))
 })
