253005472bc73ae9a79378b1e01a5dfdc337adcf
stnava <stnava@gmail.com>
2015-07-26 10:20:36 -0700

    WIP: direct to c++ for sccan/sparseDecom2 - small bug

--- R/sparseDecom2.R
+++ R/sparseDecom2.R
@@ -151,12 +151,12 @@ sparseDecom2 <- function(
   if ( perms >  0 )
   {
   ccasummary$pvalues = rep(0 , nvecs )
+  nsubs = nrow( inputMatrices[[1]] )
   for ( permer in 1:perms )
   {
-  permmatrix = list(
-    inputMatrices[[1]][ sample( 1:nrow(inputMatrices[[1]]) ) ,  ],
-    inputMatrices[[2]][ sample( 1:nrow(inputMatrices[[2]]) ) ,  ]
-    )
+  m1 = data.matrix( inputMatrices[[1]][ sample( 1:nsubs ) ,  ] )
+  m2 = data.matrix( inputMatrices[[2]][ sample( 1:nsubs ) ,  ] )
+  permmatrix = list( m1, m2 )
   sccanerp=.sparseDecom2helper2(
     permmatrix,
     inmask,
@@ -240,176 +240,6 @@ sparseDecom2 <- function(
         eig1 = t(outval$eig1),
         eig2 = t(outval$eig2),
         corrs = diag( cor( p1 , p2  ) )
-        )
-      )
-}
-
-.sparseDecom2helper <- function(
-  inputMatrices,
-  inmask,
-  sparseness,
-  nvecs,
-  its,
-  cthresh,
-  statdir,
-  uselong,
-  z,
-  smooth,
-  robust,
-  mycoption,
-  initializationList,
-  initializationList2,
-  ell1,
-  priorWeight,
-  verbose) {
-  numargs <- nargs()
-  if (numargs < 1 | missing(inputMatrices)) {
-    cat(" sparseDecom( inputMatrices=NA,  inmask=NA , sparseness=c(0.01,0.01) , nvecs=50 , cthresh=c(250,250),  its=5  ) \n")
-    cat(" each input should be a list with 2 entries e.g. sparseness=c(0.01,0.02) \n")
-    return(0)
-  }
-  if (is.na(inmask[1]) & length(initializationList) > 0) {
-    cat("You should set pass a mask in mask slot 1 if you are using initializationList\n")
-    return(0)
-  }
-  if (is.na(inmask[2]) & length(initializationList2) > 0) {
-    cat("You should set pass a mask in mask slot 2 if you are using initializationList2\n")
-    return(0)
-  }
-  if (length(cthresh) < 2) {
-    cat("You should set a length=2 cthresh value even if it is c(0,0)\n")
-    return(0)
-  }
-  post <- c(1, 2)
-  if (class(inmask[[1]])[[1]] == "antsImage") {
-    if ( sum( inmask[[1]] > 0.5 ) != ncol(inputMatrices[[1]]) )
-      stop("dimensions of view 1 mask and view 1 matrix do not match")
-  }
-  if (class(inmask[[2]])[[1]] == "antsImage") {
-    if ( sum( inmask[[2]] > 0.5 ) != ncol(inputMatrices[[2]]) )
-      stop("dimensions of view 2 mask and view 2 matrix do not match")
-  }
-  if (is.na(statdir))
-    statdir <- paste(tempdir(), "/", sep = "")
-  mfn <- paste(statdir, "sccamask", post, ".nii.gz", sep = "")
-  outfn <- paste(statdir, "scca.nii.gz", sep = "")
-  decomp <- paste(statdir, "sccaprojectionsView", post, "vec.csv", sep = "")
-  matname <- paste(statdir, "sccamatrix", post, ".mha", sep = "")
-  antsImageWrite(as.antsImage(inputMatrices[[1]]), matname[1])
-  antsImageWrite(as.antsImage(inputMatrices[[2]]), matname[2])
-  sccaname <- "two-view["
-  if (class(inmask[[1]])[[1]] == "antsImage") {
-    m1 <- inmask[[1]]
-    dim <- as.numeric(m1@dimension)
-    if (dim[1] == 4)
-      sccaname <- "dynsccan["
-    antsImageWrite(inmask[[1]], mfn[1])
-  } else mfn[1] <- NA
-  if (class(inmask[[2]])[[1]] == "antsImage") {
-    m2 <- inmask[[2]]
-    dim2 <- as.numeric(m2@dimension)
-    if (dim2[1] == 4)
-      sccaname <- "dynsccan["
-    antsImageWrite(inmask[[2]], mfn[2])
-  } else mfn[2] <- NA
-  args <- list("--scca", paste(sccaname, matname[1], ",", matname[2], ",", mfn[1],
-    ",", mfn[2], ",", sparseness[1], ",", sparseness[2], "]", sep = ""), "--l1",
-    ell1, "-i", its, "--PClusterThresh", cthresh[1], "-p", 0, "--QClusterThresh",
-    cthresh[2], "-n", nvecs, "-o", outfn, "-g", uselong, "-z", z, "-s", smooth,
-    "-r", 0, "-c", mycoption, "--prior-weight", priorWeight,"-v", verbose)
-
-  if (length(initializationList) > 0) {
-    ct <- 1
-    outfns <- c()
-    for (img in initializationList) {
-      tempfn <- paste(statdir, "init", ct, ".nii.gz", sep = "")
-      outfns <- c(outfns, tempfn)
-      antsImageWrite(img, tempfn)
-      ct <- ct + 1
-    }
-    initlistfn <- paste(statdir, "init.txt", sep = "")
-    fileConn <- file(initlistfn)
-    writeLines(outfns, fileConn)
-    close(fileConn)
-    args <- list("--scca", paste(sccaname, matname[1], ",", matname[2], ",",
-      mfn[1], ",", mfn[2], ",", sparseness[1], ",", sparseness[2], "]", sep = ""),
-      "--l1", ell1, "-i", its, "--PClusterThresh", cthresh[1], "-p", 0,
-      "--QClusterThresh", cthresh[2], "-n", nvecs, "-o", outfn, "-g", uselong,
-      "-z", z, "-s", smooth, "-r", 0, "-c", mycoption, "--mask", mfn[1],
-      "--initialization", initlistfn, "--prior-weight", priorWeight,"-v", verbose)
-    if (length(initializationList2) > 0) {
-      ct <- 1
-      outfns <- c()
-      for (img in initializationList2) {
-        tempfn <- paste(statdir, "init2_", ct, ".nii.gz", sep = "")
-        outfns <- c(outfns, tempfn)
-        antsImageWrite(img, tempfn)
-        ct <- ct + 1
-      }
-      initlistfn2 <- paste(statdir, "init2.txt", sep = "")
-      fileConn <- file(initlistfn2)
-      writeLines(outfns, fileConn)
-      close(fileConn)
-      args <- list("--scca", paste(sccaname, matname[1], ",", matname[2], ",",
-        mfn[1], ",", mfn[2], ",", sparseness[1], ",", sparseness[2], "]",
-        sep = ""), "--l1", ell1, "-i", its, "--PClusterThresh", cthresh[1],
-        "-p", 0, "--QClusterThresh", cthresh[2], "-n", nvecs, "-o", outfn,
-        "-g", uselong, "-z", z, "-s", smooth, "-r", 0, "-c", mycoption,
-        "--mask", mfn[1], "--initialization", initlistfn, "--mask2", mfn[2],
-        "--initialization2", initlistfn2, "--prior-weight", priorWeight,
-        "-v", verbose)
-    }
-  }
-  .Call("sccan", .int_antsProcessArguments(c(args)), PACKAGE = "ANTsR")
-  mydecomp <- read.csv(decomp[1])
-  if (!is.na(inmask[[1]])) {
-    glb <- paste("scca*View1vec*.nii.gz", sep = "")
-    fnl <- list.files(path = statdir, pattern = glob2rx(glb), full.names = T,
-      recursive = T)[1:nvecs]
-    fnll <- list()
-    for (i in 1:length(fnl)) {
-      img <- antsImageRead(fnl[i], dim[1])
-      fnll <- lappend(fnll, img)
-    }
-    fnl <- fnll
-    projmat1=t( imageListToMatrix( fnl, inmask[[1]] ) )
-  }
-  mydecomp2 <- read.csv(decomp[2])
-  if (!is.na(inmask[[2]])) {
-    glb <- paste("scca*View2vec*.nii.gz", sep = "")
-    fnl2 <- list.files(path = statdir, pattern = glob2rx(glb), full.names = T,
-      recursive = T)[1:nvecs]
-    fnll2 <- list()
-    for (i in 1:length(fnl2)) {
-      img <- antsImageRead(fnl2[i], dim2[1])
-      fnll2 <- lappend(fnll2, img)
-    }
-    fnl2 <- fnll2
-    projmat2=t( imageListToMatrix( fnl2, inmask[[2]] ) )
-  }
-  if (is.na(inmask[[1]])) {
-    glb <- paste("scca*_Variate_View1vec.csv", sep = "")
-    fnl <- list.files(path = statdir, pattern = glob2rx(glb), full.names = T,
-      recursive = T)
-    fnl <- read.csv(fnl)
-    projmat1 = data.matrix( fnl )
-  }
-  if (is.na(inmask[[2]])) {
-    glb <- paste("scca*_Variate_View2vec.csv", sep = "")
-    fnl2 <- list.files(path = statdir, pattern = glob2rx(glb), full.names = T,
-      recursive = T)
-    fnl2 <- read.csv(fnl2)
-    projmat2 = data.matrix( fnl2 )
-  }
-  return(
-    list(
-        projections = mydecomp,
-        projections2 = mydecomp2,
-        eig1 = fnl,
-        eig2 = fnl2,
-        corrs = diag(
-          cor( inputMatrices[[1]] %*% projmat1 ,
-               inputMatrices[[2]] %*% projmat2 ) )
         )
       )
 }
