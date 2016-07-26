c2fa718c6e92c39bbb54013209c9da23c0bd5388
stnava <stnava@gmail.com>
2016-07-26 11:13:56 -0400

    ENH: scancor

--- R/multiscaleSVDxpts.R
+++ R/multiscaleSVDxpts.R
@@ -11,7 +11,7 @@
 #' @param r radius of epsilon-ball
 #' @param kmetric similarity or distance metric determining k nearest neighbors
 #' @param eps epsilon error for rapid knn
-#' @return matrix sparse matrix is output
+#' @return matrix sparse p by p matrix is output with p by k nonzero entries
 #' @author Avants BB
 #' @references
 #' \url{http://www.math.jhu.edu/~mauro/multiscaledatageometry.html}
@@ -43,9 +43,11 @@ sparseDistanceMatrix <- function( x, k = 3, r = Inf,
 # see http://www.analytictech.com/mb876/handouts/distance_and_correlation.htm
 # euclidean distance to correlation - xin contains correlations
   ecor <- function( xin ) { 1.0 - xin^2 / ( 2 * nrow( x ) ) }
-  if ( kmetric == "covariance" ) mycov = apply( x, FUN=sd, MARGIN=2 )
-  if ( cometric ) x = scale( x )
-  bknn = nabor::knn( t( x ) , k=k, eps=eps )
+  if ( cometric ) {
+    x = scale( x )
+    y = scale( y )
+    }
+  bknn = nabor::knn( t( x ), t( y ) , k=k, eps=eps )
   if ( cometric ) bknn$nn.dists = ecor( bknn$nn.dists )
   tct = 0
   for ( i in 1:ncol( x ) )
@@ -97,4 +99,105 @@ sparseDistanceMatrix <- function( x, k = 3, r = Inf,
 #  sparsenessParam = ( range( abs( mysvd ) ) * 0.00001 )[ 2 ]
 #  sparsenessParam = 1e-6
 #
+}
+
+
+
+
+
+
+
+#' Create sparse distance, covariance or correlation matrix from x, y
+#'
+#' Exploit k-nearest neighbor algorithms to estimate a sparse matrix measuring
+#' the distance, correlation or covariance between two matched datasets.
+#' Critical to the validity of this function is the basic mathematical
+#' relationships between euclidean distance and correlation and between
+#' correlation and covariance.  For applications of such matrices, one may see
+#' relevant publications by Mauro Maggioni and other authors.
+#'
+#' @param x input matrix, should be n (samples) by p (measurements)
+#' @param y input matrix second view, should be n (samples) by q (measurements)
+#' @param k number of neighbors
+#' @param r radius of epsilon-ball
+#' @param kmetric similarity or distance metric determining k nearest neighbors
+#' @param eps epsilon error for rapid knn
+#' @return matrix sparse p by q matrix is output with p by k nonzero entries
+#' @author Avants BB
+#' @references
+#' \url{http://www.math.jhu.edu/~mauro/multiscaledatageometry.html}
+#' @examples
+#' \dontrun{
+#' mat = matrix( rnorm(60), nrow=6 )
+#' mat2 = matrix( rnorm(120), nrow=6 )
+#' smat = sparseDistanceMatrixXY( mat, mat2, 3 )
+#' smat2 = sparseDistanceMatrixXY( mat2, mat, 3 )
+#' }
+#' @export sparseDistanceMatrixXY
+sparseDistanceMatrixXY <- function( x, y, k = 3, r = Inf,
+  kmetric = c("euclidean", "correlation", "covariance"  ),
+  eps = 1.e-6 )
+{
+  if ( ! usePkg("Matrix") )
+    stop("Please install the Matrix package")
+  if ( ! usePkg("nabor") )
+    stop("Please install the nabor package")
+  kmetric <- match.arg( kmetric )
+  cometric = ( kmetric == "correlation" | kmetric == "covariance" )
+  if ( cometric & r == Inf ) r = -Inf
+#  if ( ! usePkg("irlba") )
+#    stop("Please install the irlba package")
+# see http://www.analytictech.com/mb876/handouts/distance_and_correlation.htm
+# euclidean distance to correlation - xin contains correlations
+  ecor <- function( xin ) { 1.0 - xin^2 / ( 2 * nrow( x ) ) }
+  if ( kmetric == "covariance" ) mycov = apply( x, FUN=sd, MARGIN=2 )
+  if ( cometric ) x = scale( x )
+  bknn = nabor::knn( t( y ) , t( x ), k=k, eps=eps )
+  if ( cometric ) bknn$nn.dists = ecor( bknn$nn.dists )
+  tct = 0
+  for ( i in 1:ncol( x ) )
+    {
+    inds = bknn$nn.idx[i,]    # index
+    locd = bknn$nn.dists[i,]  # dist
+    tct = tct + sum( !is.na(inds) )
+    }
+  # build triplet representation for sparse matrix
+  myijmat = matrix( nrow=(tct), ncol=3 )
+  tct2 = 1
+  for ( i in 1:ncol( x ) )
+    {
+    inds = bknn$nn.idx[i,]
+    locd = bknn$nn.dists[i,]
+    tctinc = sum( !is.na(inds) )
+    if ( kmetric == "covariance" )
+      {
+      locd = cov( x[,i], y[,inds] )
+      }
+    else if ( kmetric == "covariance" )
+      {
+      locd = cor( x[,i], y[,inds] )
+      }
+    if ( tctinc > 0 )
+      {
+      upinds = tct2:(tct2+tctinc-1)
+      myijmat[ upinds, 1 ] = i
+      myijmat[ upinds, 2 ] = inds[ !is.na( inds ) ]
+      myijmat[ upinds, 3 ] = locd[ !is.na( inds ) ]
+      tct2 = tct2 + tctinc
+      }
+    }
+  kmatSparse = Matrix::sparseMatrix(
+    i=myijmat[,1],
+    j=myijmat[,2],
+    x=myijmat[,3], symmetric = FALSE
+  )
+  if ( cometric )
+    {
+#    if ( kmetric == "covariance" )  diag( kmatSparse ) = mycov^2
+#    if ( kmetric == "correlation" ) diag( kmatSparse ) = 1
+    kmatSparse[ kmatSparse < r ] = 0
+    }  else {
+    kmatSparse[ kmatSparse > r ] = 0
+    }
+  return( kmatSparse )
 }
