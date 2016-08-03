f1a0007b6d5147eb1d5dcb9f6df589bf503af0a3
stnava <stnava@gmail.com>
2016-08-03 11:11:59 -0400

    DOC: run the multiscale SVD example

--- R/multiscaleSVDxpts.R
+++ R/multiscaleSVDxpts.R
@@ -226,7 +226,7 @@ sparseDistanceMatrixXY <- function( x, y, k = 3, r = Inf,
 #' @references
 #' \url{http://www.math.jhu.edu/~mauro/multiscaledatageometry.html}
 #' @examples
-#' \dontrun{
+#' 
 #' sphereDim = 9
 #' embeddDim = 100
 #' n = 1000
@@ -236,7 +236,7 @@ sparseDistanceMatrixXY <- function( x, y, k = 3, r = Inf,
 #' spherEmbed[ , 1:ncol( sphereData ) ] = spherEmbed[ , 1:ncol( sphereData ) ] + sphereData
 #' myr = seq( 1.0, 2.2, 0.05 ) # scales at which to sample
 #' mymssvd = multiscaleSVD( spherEmbed, myr, locn=5, nev=20, plot=1 )
-#' }
+#' 
 #' @export multiscaleSVD
 multiscaleSVD <- function( x, r, locn, nev, knn = 0, verbose=FALSE, plot=0 )
 {
