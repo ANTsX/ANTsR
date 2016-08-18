66516ea68479e37c1465fd0abf75d24eafd61cac
stnava <stnava@gmail.com>
2016-08-18 10:29:38 -0400

    ENH: antsrVol

--- R/antsrSurf.R
+++ R/antsrSurf.R
@@ -1,7 +1,7 @@
 #' Render a function onto a surface.
 #'
 #' Use a system call to \code{antsSurf} in order to render a function onto
-#' a surface.  This requires \code{antsSurf} to be in the enviornment as well
+#' a surface.  This requires \code{antsSurf} to be in the environment as well
 #' as \code{ConvertScalarImageToRGB}, both available in Advanced Normalization
 #' Tools software on github.
 #'
@@ -18,6 +18,7 @@
 #' this parameter will override \code{quantlimits}.  Currently, this will set
 #' levels above \code{overlayLimits[2]} to \code{overlayLimits[2]}.
 #' @param filename prefix filename for output pngs
+#' @param antspath pass the ANTSPATH here otherwise we try to detect it from environment
 #' @param verbose prints the command used to call \code{antsSurf}
 #' @return no output
 #' @author Avants BB, Tustison NJ
@@ -77,6 +78,7 @@ antsrSurf <- function( x, y,
   rotationParams = c(270,0,270),
   overlayLimits = NA,
   filename = NA,
+  antspath = NA,
   verbose = FALSE )
 {
 domainImageMap = NA
@@ -84,7 +86,9 @@ domainImageMap = NA
 # check for needed programs
 # first get antspath
 myantspath = Sys.getenv(c("ANTSPATH"))
-if ( length( myantspath ) == 0 ) stop("Please set ANTSPATH in your environment")
+if ( nchar( myantspath ) == 0 & is.na( antspath ) )
+  stop("Please set ANTSPATH in your environment")
+if ( ! is.na( antspath ) ) myantspath = antspath
 ass = file.exists( paste(myantspath,"antsSurf",sep='/') )
 cvt = file.exists( paste(myantspath,"ConvertScalarImageToRGB",sep='/') )
 if ( length( ass ) == 0 ) stop("This function needs antsSurf in the environment.")
@@ -173,6 +177,161 @@ asscmd = paste( asscmd , " -i ", inflationFactor,
 }
 if ( verbose ) print( asscmd )
 sss = system( asscmd )
+Sys.sleep( 3 )
+if ( nrow( rotationParams ) > 1 ) pngs[ myrot ] = pngfnloc
+}
+Sys.sleep( 3 )
+if ( nrow( rotationParams ) > 1 )
+  {
+  mypng = png::readPNG( pngs[ 1 ] )
+  for ( i in 2:length( pngs ) )
+    {
+    mypng = abind::abind(  mypng, png::readPNG( pngs[ i ] ) , along = 2 )
+    }
+  png(paste(filename, ".png", sep = ""), width = dim(mypng)[2], height = dim(mypng)[1])
+  grid::grid.raster(mypng)
+  dev.off()
+  }
+}
+
+
+
+
+
+
+
+
+
+#' Render an image volume with or without overlay.
+#'
+#' Use a system call to \code{antsVol} in order to render an image.  This
+#' requires \code{antsVol} to be in the environment as well as
+#' \code{ConvertScalarImageToRGB}, both available in Advanced Normalization
+#' Tools software on github.
+#'
+#' @param x input antsImage defining the image to render
+#' @param y input antsImage list defining the function to render
+#' on the image. these image(s) should be in the same space.
+#' @param quantlimits lower and upper quantile limits for overlay
+#' @param colormap character, one of: grey, red, green, blue, copper, jet, hsv,
+#' spring, summer, autumn, winter, hot, cool, overunder, custom
+#' @param rotationParams 3 Rotation angles expressed in degrees or a matrix of
+#' rotation parameters that will be applied in sequence.
+#' @param overlayLimits absolute lower and upper limits for functional overlay.
+#' this parameter will override \code{quantlimits}.  Currently, this will set
+#' levels above \code{overlayLimits[2]} to \code{overlayLimits[2]}.
+#' @param magnificationFactor zooms in on image during rendering
+#' @param intensityTruncation lower and upper quantiles at which to truncate intensity
+#' @param filename prefix filename for output pngs
+#' @param antspath pass the ANTSPATH here otherwise we try to detect it from environment
+#' @param verbose prints the command used to call \code{antsVol}
+#' @return no output
+#' @author Avants BB, Tustison NJ
+#' @examples
+#'
+#' \dontrun{
+#' ch2i = antsImageRead( getANTsRData("mni") )
+#' ch2seg = thresholdImage( ch2i, "Otsu", 3 )
+#' wm   = thresholdImage( ch2seg, 3, 3 )
+#' wm2 = smoothImage( wm, 1 ) %>% thresholdImage( 0.2, Inf )
+#' kimg = weingartenImageCurvature( ch2i, 1.5  ) %>% smoothImage( 1 )
+#' rp1 = matrix( c(90,180,90), ncol = 3 )
+#' rp2 = matrix( c(90,180,270), ncol = 3 )
+#' rp3 = matrix( c(90,180,180), ncol = 3 )
+#' rp  = rbind( rp1, rp3, rp2 )
+#' antsrVol( wm2, list( kimg ), quantlimits=c(0.01,0.99),rotationParams = rp )
+#' }
+#'
+#' @export antsrVol
+antsrVol <- function( x, y,
+  quantlimits = c(0.1,0.9),
+  colormap = 'jet',
+  rotationParams = c(270,0,270),
+  overlayLimits = NA,
+  magnificationFactor = 1.0,
+  intensityTruncation = c( 0.0, 1.0 ),
+  filename = NA,
+  antspath = NA,
+  verbose = FALSE )
+{
+# check for needed programs
+# first get antspath
+myantspath = Sys.getenv(c("ANTSPATH"))
+if ( nchar( myantspath ) == 0 & is.na( antspath ) )
+  stop("Please set ANTSPATH in your environment")
+if ( ! is.na( antspath ) ) myantspath = antspath
+ass = file.exists( paste(myantspath,"antsVol",sep='/') )
+cvt = file.exists( paste(myantspath,"ConvertScalarImageToRGB",sep='/') )
+if ( length( ass ) == 0 ) stop("This function needs antsSurf in the environment.")
+if ( length( cvt ) == 0  ) stop("This function needs ConvertScalarImageToRGB in the environment.")
+ass = paste(myantspath,"antsVol",sep='/')
+cvt = paste(myantspath,"ConvertScalarImageToRGB",sep='/')
+xfn = tempfile( fileext = ".nii.gz" )
+xmod = iMath( x, "TruncateIntensity", intensityTruncation[1], intensityTruncation[2] )
+antsImageWrite( xmod, xfn )
+if ( is.na( filename ) ) filename = tempfile( )
+if ( ! is.matrix( rotationParams ) )
+  rotationParams = matrix( rotationParams, ncol=3 )
+if ( nrow( rotationParams ) > 1 )
+  {
+  if ( !  usePkg("abind") | !  usePkg("png") | !  usePkg("grid"))
+    {
+    print("Need abind, grid and png")
+    return(NULL)
+    }
+  }
+pngs = rep( NA, nrow( rotationParams ) )
+for( myrot in 1:nrow( rotationParams ) )
+{
+assvcmd = paste( ass," -i  ",xfn," ",sep='')
+if ( ! missing( y ) )
+{
+ct = 0
+if ( length( colormap ) != length( y ) )
+  colormap = rep( colormap, length.out = length( y ) )
+for ( overlay in y )
+  {
+  ct = ct + 1
+  wms = smoothImage( overlay, 1.0 )
+  myquants = quantile( overlay[ abs(overlay) > 0 ], quantlimits )
+  if ( ! all( is.na( overlayLimits ) ) )
+    {
+    myquants = overlayLimits
+    overlay[ overlay < myquants[1] ] = 0
+    overlay[ overlay > myquants[2] ] = myquants[2]
+    if ( verbose ) print( myquants )
+    }
+  kblob = thresholdImage( wms, myquants[1], Inf )
+  kblobfn = tempfile( fileext = ".nii.gz" )
+  antsImageWrite( kblob, kblobfn )
+  overlayfn = tempfile(fileext = ".nii.gz" )
+  antsImageWrite( overlay, overlayfn )
+  csvlutfn = tempfile(fileext = ".csv" )
+  overlayrgbfn = tempfile(fileext = ".nii.gz" )
+  if ( verbose ) print( colormap[ct] )
+  cvtcmd = paste( cvt," 3 ",overlayfn, overlayrgbfn,
+    kblobfn, colormap[ct]," none ", myquants[1],myquants[2]," 0 255", csvlutfn )
+  sss = system( cvtcmd )
+  if ( verbose ) print( cvtcmd )
+  if ( verbose ) cat( "\n" )
+  assvcmd = paste( assvcmd , "-f [ ",overlayrgbfn,", ",kblobfn," ] ")
+  }
+}
+if ( nrow( rotationParams ) == 1 )
+{
+assvcmd = paste( assvcmd ,
+  " -d [",magnificationFactor,",",paste( rotationParams[myrot,], collapse='x' ),",255x255x255] " )
+} else {
+  pngext = myrot
+  if ( myrot < 10 ) pngext = paste( "0",pngext,sep='' )
+  if ( myrot < 100 ) pngext = paste( "0",pngext,sep='' )
+  pngfnloc = paste( filename, pngext, ".png", sep='' )
+  system( paste( "rm", pngfnloc ) )
+  assvcmd = paste( assvcmd ,  " -d ", pngfnloc,
+    "[",paste( rotationParams[myrot,], collapse='x' ),",255x255x255] ",sep='' )
+}
+if ( verbose ) print( assvcmd )
+sss = system( assvcmd )
 Sys.sleep( 3 )
 if ( nrow( rotationParams ) > 1 ) pngs[ myrot ] = pngfnloc
 }
