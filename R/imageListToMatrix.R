7e6221f3d642735dbda3c42cb1f4a36f584bd6b6
stnava <stnava@gmail.com>
2016-08-19 11:00:32 -0400

    WIP: allow downsampling in image list to matrix

--- R/imageListToMatrix.R
+++ R/imageListToMatrix.R
@@ -7,19 +7,21 @@
 #' read, in order - these are image objects, not file names.
 #' @param mask An \code{antsImage} containing a binary mask, voxels in the mask
 #' are placed in the matrix. If not provided, estimated from first image in list.
+#' If the mask is a different size than the image, the images will be downsampled
+#' and smoothed to the size of the mask.
 #' @return A matrix containing the masked data, the result of calling
 #' \code{as.numeric(image, mask)} on each input image.
 #' @author Cook PA, Avants B, Kandel BM
 #' @seealso \code{\link{matrixToImages}, \link{getMask}}
 #' @examples
-#'  img <- antsImageRead(getANTsRData('r16'), 2)
+#'  img <- antsImageRead(getANTsRData('r16') )
 #'  imglist <- list()
 #'  nvox <- dim(img)[1] * dim(img)[2]
 #'  nsubj <- 50
 #'  for(ii in 1:nsubj){
 #'    imglist[ii] <- img + rnorm(nvox, sd=mean(img[img!=0]))
 #'  }
-#'  mask <- getMask(img)
+#'  mask <- getMask(img) %>% resampleImage( c( 2,2 ) )
 #'  imgmat <- imageListToMatrix(imglist, mask)
 #'
 #' @export imageListToMatrix
@@ -35,7 +37,7 @@ imageListToMatrix <- function(imageList, mask) {
 
   listfunc <- function(x) {
     if ((sum(dim(x) - dim(mask)) != 0)) {
-      stop(paste("Dimensions of image do not match mask"))
+      x = resampleImageToTarget( x, mask, 2 ) # gaussian interpolation
     }
     as.numeric(x, mask > 0)
   }
