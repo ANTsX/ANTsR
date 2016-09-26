0b56185d8f42afc9144fac7be1c9ec11edf8e6fd
stnava <stnava@gmail.com>
2016-09-26 09:57:49 -0400

    WIP: add bkandel patch analysis

---
+++ src/PatchAnalysis.h
@@ -0,0 +1,74 @@
+struct ArgumentType
+{
+  std::string inputName;           // -i option
+  std::string maskName;            // -m option
+  std::string outProjectionName;   // -p option FIXME--used to be 'outPatchName'
+  std::string eigvecName;          // -e option
+  std::string outEigvecMatrixName; // -f option
+  std::string inEigvecMatrixName; // -g option
+  std::string outPatchName;       // -q option
+  int patchSize;                  // -s option
+  double targetVarianceExplained; // -t option
+  int numberOfSamplePatches;      // -n option
+  int verbose;                    // -v option
+  int help;                       // -h option
+  bool orientationInvariant;      // -o option
+  bool meanCenter;                // -c option
+};
+
+template < class ImageType, const int dimension >
+class TPatchAnalysis
+{
+public:
+	TPatchAnalysis( ArgumentType &  );
+	void SetArguments( ArgumentType & );
+	void ReadInputImage( void );
+	void ReadMaskImage( void );
+	void GetSamplePatchLocations( void );
+	void ExtractSamplePatches( void );
+	void ExtractAllPatches( void );
+	void LearnEigenPatches( void );
+	void WriteEigenPatches( void );
+	void WriteEigenPatchMatrix( void ); // TODO  -- done? not debugged
+	void ReadEigenPatchMatrix( void );  // TODO -- done? not debugged
+	void WritePatchMatrix( void );
+	void ReorientSamplePatches( void );
+	void ReorientAllPatches( void );
+	void ProjectOnEigenPatches( void );
+	void WriteProjections( void );
+
+private:
+	ArgumentType args;
+	typename ImageType::Pointer                 canonicalFrame; // frame to rotate all patches to
+	typename ImageType::Pointer                 inputImage;
+	typename ImageType::Pointer                 maskImage;
+	vnl_matrix < int >                          patchSeedPoints;
+	vnl_matrix< typename ImageType::PixelType > vectorizedSamplePatchMatrix;
+	std::vector< unsigned int >                  indicesWithinSphere;
+	std::vector< double >                         weights;
+	vnl_matrix< typename ImageType::PixelType > vectorizedPatchMatrix;
+	vnl_matrix< typename ImageType::PixelType > significantPatchEigenvectors;
+	vnl_matrix< typename ImageType::PixelType > patchesForAllPointsWithinMask;
+	long unsigned int                            numberOfVoxelsWithinMask;
+	vnl_matrix< typename ImageType::PixelType > eigenvectorCoefficients;
+	int paddingVoxels; // amount of padding around eigenvector for constructing images
+};
+
+template< unsigned int ImageDimension, class TRealType, class TImageType,
+  class TGradientImageType, class TInterpolator >
+vnl_vector< TRealType > ReorientPatchToReferenceFrame(
+    itk::NeighborhoodIterator< TImageType > GradientImageNeighborhood1,
+    itk::NeighborhoodIterator< TImageType > GradientImageNeighborhood2,
+    const typename TImageType::Pointer MaskImage,
+    std::vector< unsigned int > IndicesWithinSphere,
+    std::vector< double > IndexWeights,
+    const typename TGradientImageType::Pointer GradientImage1,
+    const typename TGradientImageType::Pointer GradientImage2,
+    unsigned int NumberOfValuesPerVoxel,
+    TInterpolator Interpolator
+    );
+
+template< class ImageType >
+typename ImageType::Pointer ConvertVectorToSpatialImage(
+		vnl_vector< typename ImageType::PixelType > &Vector,
+		typename ImageType::Pointer Mask);
---
+++ src/patchAnalysis.cpp
@@ -0,0 +1,117 @@
+#include <exception>
+#include <algorithm>
+#include <vector>
+#include "RcppANTsR.h"
+#include <ants.h>
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sstream>
+#include <unistd.h>
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkRegionOfInterestImageFilter.h"
+#include "vnl/vnl_matrix.h"
+#include "itkNeighborhoodIterator.h"
+#include "itkImageRegionIteratorWithIndex.h"
+#include "itkCSVNumericObjectFileWriter.h"
+#include <vnl/algo/vnl_svd.h>
+#include <itkStatisticsImageFilter.h>
+#include "itkImageRegionIterator.h"
+#include "itkLinearInterpolateImageFunction.h"
+#include "itkGradientImageFilter.h"
+#include "itkCovariantVector.h"
+#include "itkGradientRecursiveGaussianImageFilter.h"
+#include "itkBSplineInterpolateImageFunction.h"
+#include "PatchAnalysis.h"
+#include "PatchAnalysis.hxx"
+
+template< class ImageType >
+SEXP patchAnalysisHelper(
+    SEXP r_inimg,
+    SEXP r_outimg,
+    Rcpp::NumericVector sigma,
+    bool sigmaInPhysicalCoordinates,
+    unsigned int kernelwidth)
+{
+  typedef float InputPixelType;
+  typedef typename ImageType::Pointer ImagePointerType;
+  typename ImageType::Pointer inimg =
+    Rcpp::as< ImagePointerType >( r_inimg );
+  typename ImageType::Pointer outimg =
+    Rcpp::as< ImagePointerType >( r_outimg );
+
+    ArgumentType args;
+    args.inputName               = "";
+    args.maskName                = "";
+    args.outProjectionName       = "projectedPatches";
+    args.eigvecName              = "";
+    args.patchSize               = 3;
+    args.targetVarianceExplained = 0.95;
+    args.outEigvecMatrixName     = "";
+    args.inEigvecMatrixName      = "";
+    args.numberOfSamplePatches   = 1000;
+    args.verbose                 = 0;
+    args.orientationInvariant    = false;
+    args.outPatchName            = "";
+    args.meanCenter              = true;
+    args.help                    = 0;
+
+    PatchAnalysis< InputPixelType, 2 >( args );
+
+//  outimg = filter->GetOutput();
+//  r_outimg = Rcpp::wrap( outimg );
+  return( r_outimg );
+}
+
+RcppExport SEXP patchAnalysis( SEXP r_inimg,
+    SEXP r_outimg,
+    SEXP r_sigma,
+    SEXP sigmaInPhysicalCoordinates,
+    SEXP r_kernelwidth )
+{
+try
+{
+
+  Rcpp::S4 antsImage( r_inimg );
+  std::string pixeltype = Rcpp::as< std::string >( antsImage.slot( "pixeltype" ));
+  unsigned int dimension = Rcpp::as< int >( antsImage.slot( "dimension" ) );
+  bool physicalSpacing = Rcpp::as< bool >( sigmaInPhysicalCoordinates );
+  Rcpp::NumericVector sigma( r_sigma );
+  unsigned int kernelwidth = Rcpp::as< unsigned int >( r_kernelwidth );
+
+  if ( (pixeltype == "float") & ( dimension == 2 ) )
+  {
+    typedef float PixelType;
+    const unsigned int dim = 2;
+    typedef itk::Image< PixelType, dim > ImageType;
+    SEXP outimg = patchAnalysisHelper< ImageType >(
+        r_inimg, r_outimg, sigma, physicalSpacing, kernelwidth);
+    return( outimg );
+  }
+  else
+  {
+    Rcpp::stop("Unsupported image dimension or pixel type.");
+  }
+}
+
+catch( itk::ExceptionObject & err )
+{
+  Rcpp::Rcout << "ITK ExceptionObject caught!" << std::endl;
+  forward_exception_to_r( err );
+}
+catch( const std::exception& exc )
+{
+  Rcpp::Rcout << "STD ExceptionObject caught!" << std::endl;
+  forward_exception_to_r( exc );
+}
+catch( ... )
+{
+  Rcpp::stop( "C++ exception (unknown reason)");
+}
+ return Rcpp::wrap(NA_REAL); // should not be reached
+}
