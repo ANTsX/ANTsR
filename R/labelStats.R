6db6c1dc30b4d0caef447b9dc399a18fc7d3c5ef
stnava <stnava@gmail.com>
2017-02-07 14:13:56 -0500

    DOC: better documentation for labelStats

--- R/labelStats.R
+++ R/labelStats.R
@@ -3,25 +3,25 @@
 #' @usage labelStats(image, labelImage)
 #' @param image Image to calculate statistics from.
 #' @param labelImage Label image.
-#' @return Data frame with columns : 
+#' @return Data frame with one row per label (including zero) and columns :
 #'   LabelValues, Mean, Min, Max, Variance, Count, Volume
-#' @examples 
+#' @examples
+#'
 #' img <- antsImageRead( getANTsRData("r16") , 2 )
 #' img <- resampleImage( img, c(64,64), 1, 0 )
 #' mask <- getMask(img)
-#' segs1 <- atropos( d = 2, a = img, m = '[0.2,1x1]',
-#'  c = '[2,0]',  i = 'kmeans[3]', x = mask )
+#' segs1 = kmeansSegmentation( img, 3 )
 #' labelStats(img, segs1$segmentation)
-#' @export 
+#'
+#' @export
 labelStats <- function(image, labelImage){
-  if(!is.antsImage(image) | !is.antsImage(labelImage)) 
+  if(!is.antsImage(image) | !is.antsImage(labelImage))
     stop("Inputs must be antsImages.")
-  if (!all(dim(image) == dim(labelImage))) 
+  if (!all(dim(image) == dim(labelImage)))
     stop("Images must be the same size.")
   image.float <- antsImageClone(image, "float")
   labelImage.int <- antsImageClone(labelImage, "unsigned int")
   df <- .Call("labelStats", image.float, labelImage.int, PACKAGE = "ANTsR")
   df=df[ order(df$LabelValue) , ]
-  df 
+  df
 }
-
