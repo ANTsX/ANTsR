2b520b200619ad2a59ee4c3aa07ac70467ce2f3c
stnava <stnava@gmail.com>
2017-02-22 08:33:18 -0500

    ENH: allow smoothing kernel

--- R/alff.R
+++ R/alff.R
@@ -11,18 +11,28 @@
 #' @param tr the period associated with the vector x (inverse of frequency)
 #' @param detrend detrend the input time series
 #' @param takesqrt take the sqrt of the computed values
+#' @param kernel smoothing kernel in estimate, see \code{spec.pgram}
 #' @return vector is output showing ALFF and fALFF values
 #' @author Avants BB
 #' @examples
 #'
 #' mat <- matrix(rnorm(3000),ncol=50)
 #' fallf = apply( mat, FUN=alffmap, MARGIN=2 )
+#' k = kernel("daniell", rep( 2, 3 ) )
+#' fallf2 = apply( mat, FUN=alffmap, MARGIN=2, kernel=k )
 #'
 #' @export alffmap
-alffmap <- function( x, flo=0.01, fhi=0.1, tr=1, detrend=TRUE, takesqrt=FALSE )
+alffmap <- function( x, flo=0.01, fhi=0.1, tr=1,
+  detrend = TRUE,
+  takesqrt = FALSE,
+  kernel )
   {
-  temp = spec.pgram( ts( x, frequency = 1.0 / tr ), taper = 0, fast = TRUE,
-    detrend = detrend, demean = FALSE, log = "n", plot = FALSE )
+  if ( missing( "kernel") )
+    temp = spec.pgram( ts( x, frequency = 1.0 / tr ), taper = 0, fast = TRUE,
+      detrend = detrend, demean = FALSE, log = "n", plot = FALSE )
+  if ( ! missing( "kernel") )
+    temp = spec.pgram( ts( x, frequency = 1.0 / tr ), taper = 0, fast = TRUE,
+      detrend = detrend, demean = FALSE, log = "n", plot = FALSE, kernel = kernel )
   fselect = ( temp$freq >= flo & temp$freq <= fhi )
   if ( takesqrt ) denom = sqrt( sum( temp$spec ) ) else denom = sum( temp$spec )
   if ( takesqrt )
