13796da186b52cc02a757ff4f853de7ff4cc7c92
stnava <stnava@gmail.com>
2017-05-29 12:15:14 -0400

    WIP: test regsitration of c functions

---
+++ src/init.c
@@ -0,0 +1,64 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .Call calls */
+extern SEXP antsAffineInitializer(SEXP);
+extern SEXP antsJointFusion(SEXP);
+extern SEXP antsMotionCorr(SEXP);
+extern SEXP antsMotionCorrStats(SEXP, SEXP, SEXP, SEXP);
+extern SEXP centerOfMass(SEXP);
+extern SEXP CreateJacobianDeterminantImage(SEXP);
+extern SEXP eigenanatomyCpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP fastMarchingExtension(SEXP, SEXP, SEXP);
+extern SEXP fsl2antsrTransform(SEXP, SEXP, SEXP, SEXP);
+extern SEXP imagesToMatrix(SEXP, SEXP, SEXP);
+extern SEXP invariantImageSimilarity(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP itkConvolveImage(SEXP, SEXP);
+extern SEXP KellyKapowski(SEXP);
+extern SEXP LabelGeometryMeasures(SEXP);
+extern SEXP N4BiasFieldCorrection(SEXP);
+extern SEXP reflectionMatrix(SEXP, SEXP, SEXP, SEXP);
+extern SEXP reorientImage(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP robustMatrixTransform(SEXP);
+extern SEXP sccanCpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP sccanX(SEXP);
+extern SEXP timeSeriesSubtraction(SEXP, SEXP);
+extern SEXP weingartenImageCurvature(SEXP, SEXP, SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+    {"antsAffineInitializer",          (DL_FUNC) &antsAffineInitializer,           1},
+    {"antsJointFusion",                (DL_FUNC) &antsJointFusion,                 1},
+    {"antsMotionCorr",                 (DL_FUNC) &antsMotionCorr,                  1},
+    {"antsMotionCorrStats",            (DL_FUNC) &antsMotionCorrStats,             4},
+    {"centerOfMass",                   (DL_FUNC) &centerOfMass,                    1},
+    {"CreateJacobianDeterminantImage", (DL_FUNC) &CreateJacobianDeterminantImage,  1},
+    {"eigenanatomyCpp",                (DL_FUNC) &eigenanatomyCpp,                15},
+    {"fastMarchingExtension",          (DL_FUNC) &fastMarchingExtension,           3},
+    {"fsl2antsrTransform",             (DL_FUNC) &fsl2antsrTransform,              4},
+    {"imagesToMatrix",                 (DL_FUNC) &imagesToMatrix,                  3},
+    {"invariantImageSimilarity",       (DL_FUNC) &invariantImageSimilarity,       11},
+    {"itkConvolveImage",               (DL_FUNC) &itkConvolveImage,                2},
+    {"KellyKapowski",                  (DL_FUNC) &KellyKapowski,                   1},
+    {"LabelGeometryMeasures",          (DL_FUNC) &LabelGeometryMeasures,           1},
+    {"N4BiasFieldCorrection",          (DL_FUNC) &N4BiasFieldCorrection,           1},
+    {"reflectionMatrix",               (DL_FUNC) &reflectionMatrix,                4},
+    {"reorientImage",                  (DL_FUNC) &reorientImage,                   6},
+    {"robustMatrixTransform",          (DL_FUNC) &robustMatrixTransform,           1},
+    {"sccanCpp",                       (DL_FUNC) &sccanCpp,                       19},
+    {"sccanX",                         (DL_FUNC) &sccanX,                          1},
+    {"timeSeriesSubtraction",          (DL_FUNC) &timeSeriesSubtraction,           2},
+    {"weingartenImageCurvature",       (DL_FUNC) &weingartenImageCurvature,        3},
+    {NULL, NULL, 0}
+};
+
+void R_init_ANTsR(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
