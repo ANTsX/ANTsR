a741cfe5a8ffb877e5703bbaee54e33e30969952
stnava <stnava@gmail.com>
2017-06-07 07:06:05 -0400

    ENH: reflection metric

--- R/reflectImage.R
+++ R/reflectImage.R
@@ -5,7 +5,9 @@
 #' @param img1 input object, an antsImage
 #' @param axis which dimension to reflect across, numbered from 0 to imageDimension-1
 #' @param tx transformation type to estimate after reflection
+#' @param metric similarity metric for image registration.  see \code{antsRegistration}.
 #' @author BB Avants
+#' @seealso \code{\link{antsRegistration}}
 #' @examples
 #'
 #' fi<-antsImageRead( getANTsRData("r16") , 2 )
@@ -14,7 +16,7 @@
 #' asym<-asym-fi
 #'
 #' @export reflectImage
-reflectImage<-function( img1, axis=NA, tx=NA ) {
+reflectImage<-function( img1, axis=NA, tx=NA, metric="mattes" ) {
   if ( is.na(axis) ) axis=( img1@dimension - 1 )
   if ( axis > img1@dimension | axis < 0 ) axis=(img1@dimension-1)
 
@@ -23,7 +25,8 @@ reflectImage<-function( img1, axis=NA, tx=NA ) {
 
   if ( ! is.na(tx) )
   {
-  rfi<-invisible( antsRegistration(img1,img1,typeofTransform = tx,
+  rfi<-invisible( antsRegistration( img1, img1, typeofTransform = tx,
+    synMetric = metric,
     outprefix = tempfile(),
     initialTransform = rflct ) )
   return( rfi )
