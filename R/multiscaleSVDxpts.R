3a4e08c0dc6c8c84e9b6a8895d7c54f6e9e91b20
stnava <stnava@gmail.com>
2017-06-18 13:06:02 -0400

    BUG: print statements

--- R/multiscaleSVDxpts.R
+++ R/multiscaleSVDxpts.R
@@ -668,14 +668,12 @@ knnSmoothImage <- function(
 #  imat = antsrimpute(
 #    getNeighborhoodInMask( img, mask, rep( r, img@dimension), boundary.condition='image' ) )
 #  imat = knnSmoothingMatrix( imat, k = 2*(r*2+1)^2, sigma = intensitySigma )
-print("a")
   jmat = knnSmoothingMatrix( spatmat, k = (r*2+1)^2, sigma = spatialSigma )
 #  return( jmat )
 #  image( jmat[4000:4500,4000:4500] )
 #  print( jmat[4000:4010,4000:4010] )
 #  imat = imat / Matrix::rowSums( imat )
 #  jmat = imat * smoothingMatrix
-print("b")
   for ( i in 1:4 ) { # sinkhorn
     jmat = jmat / Matrix::rowSums( jmat )
     jmat = Matrix::t( Matrix::t(jmat) / Matrix::rowSums( Matrix::t(jmat) ) )
