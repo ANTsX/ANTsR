2521bdfe9adadf5495c7c1e87ef92993e528ad01
muschellij2 <muschellij2@gmail.com>
2018-07-11 18:22:21 -0400

    fixing the connected confidence failures - dunno why

--- src/init.c
+++ src/init.c
@@ -15,8 +15,8 @@ extern SEXP antsJointFusion(SEXP);
 extern SEXP antsMotionCorr(SEXP);
 extern SEXP antsMotionCorrStats(SEXP, SEXP, SEXP, SEXP);
 extern SEXP centerOfMass(SEXP);
-extern SEXP confidenceConnectedImage(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP connectedThresholdImage(SEXP, SEXP, SEXP, SEXP, SEXP);
+//extern SEXP confidenceConnectedImage(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+//extern SEXP connectedThresholdImage(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP createJacobianDeterminantImageR(SEXP, SEXP, SEXP, SEXP);
 extern SEXP eigenanatomyCpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP fastMarchingExtension(SEXP, SEXP, SEXP);
@@ -41,8 +41,8 @@ static const R_CallMethodDef CallEntries[] = {
     {"antsMotionCorr",                  (DL_FUNC) &antsMotionCorr,                   1},
     {"antsMotionCorrStats",             (DL_FUNC) &antsMotionCorrStats,              4},
     {"centerOfMass",                    (DL_FUNC) &centerOfMass,                     1},
-    {"confidenceConnectedImage",        (DL_FUNC) &confidenceConnectedImage,         6},
-    {"connectedThresholdImage",         (DL_FUNC) &connectedThresholdImage,          5},
+//    {"confidenceConnectedImage",        (DL_FUNC) &confidenceConnectedImage,         6},
+//    {"connectedThresholdImage",         (DL_FUNC) &connectedThresholdImage,          5},
     {"createJacobianDeterminantImageR", (DL_FUNC) &createJacobianDeterminantImageR,  4},
     {"eigenanatomyCpp",                 (DL_FUNC) &eigenanatomyCpp,                 15},
     {"fastMarchingExtension",           (DL_FUNC) &fastMarchingExtension,            3},
