d8a495381d29efc41319ddc905c212882ea55c42
stnava <stnava@gmail.com>
2019-10-28 15:13:05 -0400

    ENH: options to reflect image (#285)
    * ENH: options to reflect image
    
    * WIP: RcppHNSW

--- R/reflectImage.R
+++ R/reflectImage.R
@@ -6,9 +6,10 @@
 #' @param axis which dimension to reflect across, numbered from 0 to imageDimension-1
 #' @param tx transformation type to estimate after reflection
 #' @param metric similarity metric for image registration.  see \code{antsRegistration}.
-#' @param verbose print diagnostic messages, passed to 
+#' @param verbose print diagnostic messages, passed to
 #' \code{\link{antsRegistration}} and \code{\link{antsApplyTransforms}}
-#' 
+#' @param ... Additional options to pass to \code{\link{antsRegistration}}
+#'
 #' @author BB Avants
 #' @seealso \code{\link{antsRegistration}}
 #' @examples
@@ -20,24 +21,26 @@
 #'
 #' @export reflectImage
 reflectImage<-function(img1, axis=NA, tx=NA, metric="mattes",
-                        verbose = FALSE) {
+verbose = TRUE,
+... ) {
   if ( is.na(axis) ) {
     axis=( img1@dimension - 1 )
   }
   if ( axis > img1@dimension | axis < 0 ) {
     axis=(img1@dimension-1)
   }
-  
+
   rflct<-tempfile(fileext = ".mat")
   catchout = .Call( "reflectionMatrix", img1, axis, rflct, package="ANTsR")
-  
+
   if ( ! all(is.na(tx) ))
   {
     rfi <- antsRegistration( img1, img1, typeofTransform = tx,
                              synMetric = metric,
                              outprefix = tempfile(),
                              initialTransform = rflct,
-                             verbose = verbose) 
+                             verbose = verbose,
+                             ...)
     return( rfi )
   } else {
     return( antsApplyTransforms( img1, img1, rflct, verbose = verbose)  )
