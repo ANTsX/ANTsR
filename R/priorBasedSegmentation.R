ecfb0b32e340c8b596958a8a0561cf4f310903fa
stnava <stnava@gmail.com>
2021-11-04 08:48:44 -0400

    DOC: allow verbose mode

--- R/priorBasedSegmentation.R
+++ R/priorBasedSegmentation.R
@@ -9,6 +9,7 @@
 #' @param priorWeight usually 0 (priors used for initialization only), 0.25 or 0.5.
 #' @param mrf regularization, higher is smoother, a numerical value in range 0.0 to 0.2
 #' @param iterations maximum number of iterations.  could be a large value eg 25.
+#' @param verbose maximum number of iterations.  could be a large value eg 25.
 #' @return segmentation and probability images
 #' @author Brian B. Avants
 #' @examples
@@ -25,7 +26,8 @@ priorBasedSegmentation <- function(
   mask,
   priorWeight = 0.25,
   mrf = 0.1,
-  iterations = 25
+  iterations = 25,
+  verbose=FALSE
   ) {
   if ( class( img )[1] == 'antsImage' ) dim <- img@dimension
   if ( class( img )[1] == 'list' ) dim <- img[[1]]@dimension
@@ -33,6 +35,7 @@ priorBasedSegmentation <- function(
   mrf <- paste("[", mrf, ",", nhood, "]")
   conv <- paste("[", iterations, ",", 0, "]")
   pseg <- atropos( a = img, m = mrf,
-      c = conv,  i = priors, x = mask, priorweight = priorWeight )
+      c = conv,  i = priors, x = mask,
+      priorweight = priorWeight, verbose=verbose )
   return( pseg )
 }
