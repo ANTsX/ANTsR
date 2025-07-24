a8f6c09d64ee2652fbd2d2951bc234fdee230431
stnava <stnava@gmail.com>
2025-07-23 21:11:26 -0400

    PERF: rvcoef

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,4 @@
 .onAttach <- function(lib, pkg) {
   txt <- paste(pkg, utils::packageDescription(pkg, lib)[["Version"]])
-  txt <- paste0(
-    txt, "\n",
-    "Reproducibility issues in some methods. See\n",
-    "https://github.com/ANTsX/ANTs/wiki/",
-    "antsRegistration-reproducibility-issues\nfor more information.",
-    "Also see *repro methods in antsRegistration."
-  )
   packageStartupMessage(txt)
 }
