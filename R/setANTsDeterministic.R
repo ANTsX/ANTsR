dc908806ed410f4f73db1181d47110b351d0f03c
stnava <stnava@gmail.com>
2026-05-10 14:10:07 -0400

    WIP: ... deterministic

---
+++ R/setANTsDeterministic.R
@@ -0,0 +1,20 @@
+#' Set ANTs deterministic behavior
+#'
+#' @param on Set deterministic behavior.
+#' @param seedValue Assign random seed value.
+#' @return No return value, called for side effects.
+#' @examples
+#' setANTsDeterministic(TRUE)
+#' @export
+setANTsDeterministic <- function( on = TRUE, seedValue = 123 ) 
+{
+  options( ants.deterministic = on )
+  if( on ) {
+    Sys.setenv( ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS = "1" )
+    if ( !is.null( seedValue ) ) {
+      set.seed( seedValue )
+      options( ants.randomSeed = seedValue )
+      Sys.setenv( ANTS_RANDOM_SEED = seedValue )
+    }
+  }
+}
