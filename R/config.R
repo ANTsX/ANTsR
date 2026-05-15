8c15b58fc9257edf2dc466dcf88f81fd3a5ae892
stnava <stnava@gmail.com>
2026-05-15 17:25:31 -0400

    DOC: ...

--- R/config.R
+++ R/config.R
@@ -13,3 +13,26 @@
   } 
   invisible()
 }
+
+#' Set ANTs deterministic behavior
+#'
+#' @name setANTsDeterministic
+#' @rdname setANTsDeterministic
+#' @param on Set deterministic behavior.
+#' @param seedValue Assign random seed value.
+#' @return No return value, called for side effects.
+#' @examples
+#' setANTsDeterministic(TRUE)
+#' @export
+setANTsDeterministic <- function( on = TRUE, seedValue = 123 ) 
+{
+  options( ants.deterministic = on )
+  if( on ) {
+    Sys.setenv( ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS = "1" )
+    if ( !is.null( seedValue ) ) {
+      set.seed( seedValue )
+      options( ants.randomSeed = seedValue )
+      Sys.setenv( ANTS_RANDOM_SEED = seedValue )
+    }
+  }
+}
